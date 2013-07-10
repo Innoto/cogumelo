60fb5b23bd0286c0f2fb48389f30981be47c99d5
pblanco <pblanco@H2O2>
2013-07-10 14:12:18 +0200

    Arranxos en MysqlDAO

--- c_packages/base_app/c_app/classes/model/mysql/init.sql
+++ c_packages/base_app/c_app/classes/model/mysql/init.sql
@@ -14,3 +14,14 @@ CREATE TABLE  `useradmin` (
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Administrator Users Table';
 
 INSERT INTO `useradmin`(`login`,`passwd`, `name`) VALUES ('admin',SHA1('admin'), 'Administrador');
+
+
+
+DROP TABLE IF EXISTS `cousa`;
+CREATE TABLE  `cousa` (
+  `id` bigint(20) NOT NULL auto_increment,  
+  `name` varchar(30) default NULL,
+  `fingers` int(10) default NULL,
+  `hobby` varchar(30) default NULL,
+  PRIMARY KEY  USING BTREE (`id`)  	
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='cousa table';
