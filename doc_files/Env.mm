844241acd9eeb1e2483ef3ddf7862d0fbbf9ad11
mjperez <mjperez@cuchufella>
2013-07-11 10:58:20 +0200

    imaxes wiki

---
+++ doc_files/Env.mm
@@ -0,0 +1,36 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1364327057790" ID="ID_1154777549" MODIFIED="1364327376918" TEXT="Workspace">
+<icon BUILTIN="folder"/>
+<node CREATED="1364327248741" ID="ID_847567221" MODIFIED="1364328006673" POSITION="right" TEXT="Cogumelo_1.x (Core)">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1364327297164" ID="ID_1781587653" MODIFIED="1364327973098" POSITION="left" TEXT="Project1">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1364327321028" ID="ID_445732888" MODIFIED="1364328020699" POSITION="right" TEXT="Project2">
+<icon BUILTIN="folder"/>
+<node CREATED="1364327954378" ID="ID_974029719" MODIFIED="1364328080994" TEXT="c_app (all app code)">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1364328033460" ID="ID_658181234" MODIFIED="1364328097622" TEXT="httpdocs">
+<icon BUILTIN="folder"/>
+<node CREATED="1364328057509" ID="ID_557063532" MODIFIED="1364328110118" TEXT="index.php">
+<icon BUILTIN="edit"/>
+</node>
+<node CREATED="1364328091639" ID="ID_1795755776" MODIFIED="1364328105624" TEXT=".htaccess">
+<icon BUILTIN="edit"/>
+</node>
+</node>
+</node>
+<node CREATED="1364327332674" ID="ID_459319163" MODIFIED="1364327976995" POSITION="left" TEXT="Project3">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1364327343644" ID="ID_152010144" MODIFIED="1364328023792" POSITION="right" TEXT="Project4">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1364327353526" ID="ID_854650983" MODIFIED="1364327979438" POSITION="left" TEXT="Project5">
+<icon BUILTIN="folder"/>
+</node>
+</node>
+</map>
---
+++ doc_files/module.mm
@@ -0,0 +1,31 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1364298828979" ID="ID_158446820" MODIFIED="1364303099434" TEXT="ExampleModule">
+<node CREATED="1364299843892" ID="ID_768397042" MODIFIED="1364302358967" POSITION="left" TEXT="m_classes">
+<node CREATED="1364300401313" ID="ID_1721306838" MODIFIED="1364318529483" TEXT="models"/>
+<node CREATED="1364300415335" ID="ID_1799767644" MODIFIED="1364318544195" TEXT="views"/>
+<node CREATED="1364300488788" ID="ID_1190404774" MODIFIED="1364318547874" TEXT="controllers"/>
+</node>
+<node CREATED="1364299850598" ID="ID_378227033" MODIFIED="1364302278478" POSITION="right" TEXT="m_templates">
+<node CREATED="1364302278472" ID="ID_447154939" MODIFIED="1364302300600" TEXT="media">
+<node CREATED="1364301203505" ID="ID_1480395568" MODIFIED="1364301214533" TEXT="script"/>
+<node CREATED="1364301227061" ID="ID_1300336829" MODIFIED="1364301228624" TEXT="style"/>
+<node CREATED="1364301235382" ID="ID_847705465" MODIFIED="1364301239102" TEXT="img"/>
+</node>
+<node CREATED="1364302312163" ID="ID_78195377" MODIFIED="1364302314799" TEXT="template.tpl"/>
+</node>
+<node CREATED="1364299745606" ID="ID_1440933831" MODIFIED="1364302921021" POSITION="right" TEXT="module.inc">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Constantes:</b><br />- Urls<br />- Path modulo
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
