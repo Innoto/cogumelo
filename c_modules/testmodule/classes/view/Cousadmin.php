479d06691f969cee401cb557a2b5ddf28618e40b
pablinhob <pablinho@cativo.(none)>
2013-09-04 00:01:45 +0200

    Cousadminn sen caché, para poder facer mellor as probas

--- c_modules/testmodule/classes/view/Cousadmin.php
+++ c_modules/testmodule/classes/view/Cousadmin.php
@@ -22,7 +22,7 @@ class Cousadmin extends View
 
 
 	function lista() {
-		$cousas = $this->cousacontrol->listItems(false, false, false, true);
+		$cousas = $this->cousacontrol->listItems(false, false, false, false);
 
 		while($cou = $cousas->fetch()) {
 			echo "<br>";
