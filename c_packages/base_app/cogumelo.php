3bfec4d330bf235fb4c298c3ba22163f96152ad5
mjperez <mjperez@Centolo>
2014-05-15 17:04:38 +0200

    cambios cogumelo.php

--- c_packages/base_app/cogumelo.php
+++ c_packages/base_app/cogumelo.php
@@ -5,14 +5,14 @@
   //
 
   // Project location
- define('SITE_PATH', getcwd().'/c_app/');
+  define('SITE_PATH', getcwd().'/c_app/');
   $_SERVER['HTTPS'] = false;
   $_SERVER['HTTP_HOST'] = '';
   $_SERVER['REQUEST_URI'] = 'cogumelo shell script';
   $_SERVER['REMOTE_ADDR'] = "local_shell";
 
   // cogumelo core Location
- set_include_path('.:'.SITE_PATH);
+  set_include_path('.:'.SITE_PATH);
 
   require_once("conf/setup.dev.php"); 
 
@@ -22,51 +22,7 @@
   global $_C;
   $_C =Cogumelo::get();
 
- 
- if ($argc>1){ //parameters handler
-    switch($argv[1]){
-      case 'flush': // borra temporais 
-	exec('rm tmp/*');
-	echo 'archivos temporales borrados';
-        break; 
-      case 'create_db': // crea a base de datos
-        echo 'create database';
-        break; 
-      case 'generate_tables': // xenera as táboas da bd 
-        echo 'generate db tables';
-        break;  
-      case 'bck': // fai backup da bd no ficheiro indicado
-	if ($argc>2){
-	  $bd = $argv[2];
-	  if ($argc>3){ //nome do ficheiro de bck
-            $file = $argv[3];
-          }
-          else{ //crearase un bck.sql co nome da bbdd
-            $file = $bd.'.sql';
-          }
-	}
-	else{
-	  echo "you must specify a database";
-	}
-       
-        echo $file;
-        break;  
-      case 'restore': // restaura a bd pasada
-        if ($argc>2)
-	  $bd = $argv[2];
-	else
-	  echo "you must specify a database";
-        break;  
-      default:
-        echo 'invalid parameter';
-        exit;     
-    }//end switch 
-}//end parameters handler
-else{
-  echo 'execución sen parámetros';
-}
-
-//
+  //
 //  CARGO MÓDULO DEVEL E O DEVELDBCONTROLLER, QUE É ONDE ESTÁN OS MÉTODOS QUE NOS INTERESAN
 //
 require_once(COGUMELO_LOCATION."/c_modules/devel/devel.php");
@@ -75,8 +31,146 @@ require_once(COGUMELO_LOCATION."/c_modules/devel/classes/controller/DevelDBContr
 //
 //  UTILIZO A CLASE COMO FARÍA DENDE UN VIEW NORMAL
 //
-
 $develdbcontrol = new DevelDBController();
-var_dump( $develdbcontrol->getTablesSQL() ); // este método, por exemplo, devolvenos todo o  SQL xerado para os VO's do proxecto
+//var_dump( $develdbcontrol->getTablesSQL() ); // este método, por exemplo, devolvenos todo o  SQL xerado para os VO's do proxecto
+
+
+ 
+ if ($argc>1){ //parameters handler
+    switch($argv[1]){
+
+      case 'flush': // delete temporary files
+	       exec('rm tmp/');
+	       echo "archivos temporales borrados\n";
+         break; 
+
+      case 'create_db': // create database
+          $db = ReadStdin('Please enter the database: ', '');
+          $user =  ReadStdin('Enter the username: ', '');
+          //echo $db."\n";
+          // Get the password
+          fwrite(STDOUT, "Password: ");
+          $password = getPassword(true);
+          // Output the password
+          //echo "\nYour password: " . $password . "\n";
+          createDB();
+          echo "database '.$db.' created\n";
+        break; 
+
+      case 'generate_tables': // create database tables
+        createTables();
+        echo 'generate db tables';
+        break;  
+
+      case 'bck': // do the backup of the given db
+      	if ($argc>2){
+      	  $bd = $argv[2];
+      	  if ($argc>3){ //nome do ficheiro de bck
+              $file = $argv[3];
+          }
+          else{ //crearase un bck.sql co nome da bbdd
+              $file = $bd.'.sql';
+          }
+          doBackup($bd, $file);
+      	}
+      	else{
+      	  echo "Not enough parameters\n usage: bck DB file\n";
+      	}
+        break;  
+
+      case 'restore': // restore the backup of a given db
+        if ($argc>2){
+	       $bd = $argv[2];
+         retoreDB($bd);
+        }
+        else
+          echo "You must especify the database to retore\n"; 
+        break;  
+
+      default:
+        echo "invalid parameter;try: 
+        flush           to remove temporary files
+        crete_db        to create a database
+        restore_db      to restore a database";
+        exit;     
+    }//end switch 
+}//end parameters handler
+else{
+  echo "Not enough arguments;try: 
+        flush           to remove temporary files
+        crete_db        to create a database
+        restore_db      to restore a database";
+}
+
+function createDB(){
+  $develdbcontrol = new DevelDBController();
+  $develdbcontrol->createSchemaDB();
+}
+
+function createTables(){
+  $develdbcontrol = new DevelDBController();
+  $develdbcontrol->createTables();
+}
+
+function doBackup($DB, $file){
+   echo "fai o bck...";
+}
+
+/**
+ * Get data from the shell.
+ */
+function ReadStdin($prompt, $valid_inputs, $default = '') { 
+    while(!isset($input) || (is_array($valid_inputs) && !in_array($input, $valid_inputs)) || ($valid_inputs == 'is_file' && !is_file($input))) { 
+        echo $prompt; 
+        $input = strtolower(trim(fgets(STDIN))); 
+        if(empty($input) && !empty($default)) { 
+            $input = $default; 
+        } 
+    } 
+    return $input; 
+} 
+
+/**
+ * Get a password from the shell.
+ * This function works on *nix systems only and requires shell_exec and stty.
+ *
+ * @param  boolean $stars Wether or not to output stars for given characters
+ * @return string
+ */
+function getPassword($stars = false)
+{
+    // Get current style
+    $oldStyle = shell_exec('stty -g');
+
+    if ($stars === false) {
+        shell_exec('stty -echo');
+        $password = rtrim(fgets(STDIN), "\n");
+    } else {
+        shell_exec('stty -icanon -echo min 1 time 0');
+
+        $password = '';
+        while (true) {
+            $char = fgetc(STDIN);
+
+            if ($char === "\n") {
+                break;
+            } else if (ord($char) === 127) {
+                if (strlen($password) > 0) {
+                    fwrite(STDOUT, "\x08 \x08");
+                    $password = substr($password, 0, -1);
+                }
+            } else {
+                fwrite(STDOUT, "*");
+                $password .= $char;
+            }
+        }
+    }
+
+    // Reset old style
+    shell_exec('stty ' . $oldStyle);
+
+    // Return the password
+    return $password;
+}
 
 ?>
