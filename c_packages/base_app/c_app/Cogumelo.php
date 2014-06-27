efe2a938212ff7a90fc49a745230d26e362faae8
pablinho <pablinho.blanco@gmail.com>
2014-06-27 11:42:54 +0200

    solucionado error en devel + tabulados archivos

--- c_packages/base_app/c_app/Cogumelo.php
+++ c_packages/base_app/c_app/Cogumelo.php
@@ -2,29 +2,29 @@
 
 
 class Cogumelo extends CogumeloClass
-{	
-	
-	function __construct()
-	{
-		parent::__construct();
+{ 
+  
+  function __construct()
+  {
+    parent::__construct();
 
-	}
-	
-	var $url_patterns = array(
-		/*
-		'#^cousa/mostrar\/?(.*)$#' => 'view:Cousadmin::mostra_cousa',
-		'#^cousa/crear$#' => 'view:Cousadmin::crea',
-		'#^cousa$#' => 'view:Cousadmin::lista',
+  }
+  
+  var $url_patterns = array(
+    /*
+    '#^cousa/mostrar\/?(.*)$#' => 'view:Cousadmin::mostra_cousa',
+    '#^cousa/crear$#' => 'view:Cousadmin::crea',
+    '#^cousa$#' => 'view:Cousadmin::lista',
 */
-		'#^admin\/?(.*)$#' => 'view:Adminview::metodo',
-		//'#^dev$#' => 'view:DevView::main',
-		'#^404$#' => 'view:Adminview::page404',
-		// default views
+    '#^admin\/?(.*)$#' => 'view:Adminview::metodo',
+    //'#^dev$#' => 'view:DevView::main',
+    '#^404$#' => 'view:Adminview::page404',
+    // default views
 
-		'#^getobj$#' => 'view:Adminview::getobj',
-		'#^setobj$#' => 'view:Adminview::setobj',
-		'#^createdb$#' => 'view:Adminview::create_user_db_tables',
-		'#^$#' => 'view:Adminview::seccion' // App home url
-	);
+    '#^getobj$#' => 'view:Adminview::getobj',
+    '#^setobj$#' => 'view:Adminview::setobj',
+    '#^createdb$#' => 'view:Adminview::create_user_db_tables',
+    '#^$#' => 'view:Adminview::seccion' // App home url
+  );
 
-}	
+} 
--- c_packages/base_app/c_app/classes/model/UseradminVO.php
+++ c_packages/base_app/c_app/classes/model/UseradminVO.php
@@ -9,98 +9,98 @@ define('USERADMIN_LEVEL_LOW',1);
 
 
 class UseradminVO extends VO
-{	
-	static $tableName = 'useradmin';
-	static $cols = array(
-		'id' => array(
-			'type'=>'INT', 
-			'primarykey'=>true,
-			'autoincrement'=>true
-		),
-		'login' => array(
-			'name' => 'Alias', 
-			'type'=>'CHAR', 
-			'size'=>10
-		),
-		'passwd'=> array(
-			'name' => 'Contraseña', 
-			'type'=>'CHAR',
-			'size' => '30'
-		),
-		'name'=> array(
-			'name' => 'Nombre', 
-			'type'=>'CHAR', 
-			'size'=>50
-		),
-		'time_lastlogin' => array(
-			'name' => 'Último acceso', 
-			'type'=>'DATETIME'
-		),
-		'userdata_id' => array(
-			'name' => 'Datos extendidos',
-			'type' => 'INT',
-			'foreign_key' => 'id'
-		)
-	);
+{ 
+  static $tableName = 'useradmin';
+  static $cols = array(
+    'id' => array(
+      'type'=>'INT', 
+      'primarykey'=>true,
+      'autoincrement'=>true
+    ),
+    'login' => array(
+      'name' => 'Alias', 
+      'type'=>'CHAR', 
+      'size'=>10
+    ),
+    'passwd'=> array(
+      'name' => 'Contraseña', 
+      'type'=>'CHAR',
+      'size' => '30'
+    ),
+    'name'=> array(
+      'name' => 'Nombre', 
+      'type'=>'CHAR', 
+      'size'=>50
+    ),
+    'time_lastlogin' => array(
+      'name' => 'Último acceso', 
+      'type'=>'DATETIME'
+    ),
+    'userdata_id' => array(
+      'name' => 'Datos extendidos',
+      'type' => 'INT',
+      'foreign_key' => 'id'
+    )
+  );
 
 
 
-	function __construct($datarray = array())
-	{
-		parent::__construct($datarray);
-	}
-	
+  function __construct($datarray = array())
+  {
+    parent::__construct($datarray);
+  }
+  
 
 
-	///////////////////////////////////////
-	///////	Aditional getters	//////////
-	/////////////////////////////////////
+  ///////////////////////////////////////
+  /////// Aditional getters //////////
+  /////////////////////////////////////
 
-	function isLevelHigh()
-	{
-	  return($this->getter('level') == USERADMIN_LEVEL_HIGH);
-	}
-	
-	function isLevelMedium()
-	{
-	  return($this->getter('level') == USERADMIN_LEVEL_MEDIUM);
-	}
-	
-	function isLevelLow()
-	{
-	  return($this->getter('level') == USERADMIN_LEVEL_LOW);
-	}
-	
-	function getTimeLastLogin_print()
-	{
-		$date = explode( ' ', $this->getter('time_lastlogin'));
-		$datepart = explode( '-', $date[0]);
-		$day = $datepart[2]; $month = $datepart[1]; $year = $datepart[0];
-		return($day."/".$month."/".$year);
-	}
-	
-	///////////////////////////////////////
-	///////	Set	Methods	//////////////////
-	/////////////////////////////////////	
+  function isLevelHigh()
+  {
+    return($this->getter('level') == USERADMIN_LEVEL_HIGH);
+  }
+  
+  function isLevelMedium()
+  {
+    return($this->getter('level') == USERADMIN_LEVEL_MEDIUM);
+  }
+  
+  function isLevelLow()
+  {
+    return($this->getter('level') == USERADMIN_LEVEL_LOW);
+  }
+  
+  function getTimeLastLogin_print()
+  {
+    $date = explode( ' ', $this->getter('time_lastlogin'));
+    $datepart = explode( '-', $date[0]);
+    $day = $datepart[2]; $month = $datepart[1]; $year = $datepart[0];
+    return($day."/".$month."/".$year);
+  }
+  
+  ///////////////////////////////////////
+  /////// Set Methods //////////////////
+  ///////////////////////////////////// 
 
-	function setLevelHigh()
-	{
-		$this->setter('level', USERADMIN_LEVEL_HIGH);
-	}
-	
-	function setLevelMedium()
-	{
-		$this->setter('level', USERADMIN_LEVEL_MEDIUM);
-	}
-	
-	function setLevelLow()
-	{
-		$this->setter('level', USERADMIN_LEVEL_LOW);
-	}
-	
-	function setTimeLastLogin($timeLastLogin)
-	{
-		$this->setter('time_lastlogin', $timeLastLogin);
-	}
+  function setLevelHigh()
+  {
+    $this->setter('level', USERADMIN_LEVEL_HIGH);
+  }
+  
+  function setLevelMedium()
+  {
+    $this->setter('level', USERADMIN_LEVEL_MEDIUM);
+  }
+  
+  function setLevelLow()
+  {
+    $this->setter('level', USERADMIN_LEVEL_LOW);
+  }
+  
+  function setTimeLastLogin($timeLastLogin)
+  {
+    $this->setter('time_lastlogin', $timeLastLogin);
+  }
 }
 ?>
