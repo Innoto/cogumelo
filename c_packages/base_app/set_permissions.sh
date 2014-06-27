ee61864f548ecce316ea5b189d33aa3994e5d7e5
Jose M. Porto <jmporto@innoto.es>
2014-06-27 11:43:19 +0200

    Actualizando los permisos en base_app

--- c_packages/base_app/set_permissions.sh
+++ c_packages/base_app/set_permissions.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-EU=$USER
+CGUSER=$USER
+CGSERV=www-data
 
-mkdir c_app/tmp
-mkdir c_app/tmp/templates_c
-mkdir c_app/log
-mkdir c_app/backups
 
-sudo chown -R $EU:www-data httpdocs
-chmod -R go-rwx,g+rX httpdocs
+mkdir -p c_app/tmp
+mkdir -p c_app/tmp/templates_c
+mkdir -p c_app/log
+mkdir -p c_app/backups
 
-sudo chown -R $EU:www-data c_app
-chmod -R g-x,o-wx,g+rwX,o+rX c_app
-chmod -R gu+rwX,o+rX c_app/tmp c_app/log
-sudo chown $EU:$EU c_app/backups
 
-echo .
-echo 'READY. Enjoy :)'
-echo .
+sudo chown -R $CGUSER:$CGUSER cogumelo cogumelo.php set_permissions.sh
+chmod 700 cogumelo cogumelo.php set_permissions.sh
+
+sudo chown -R $CGUSER:$CGSERV c_app
+chmod -R ug-x,g-w,o-rwx,u+rwX,g+rX c_app
+chmod -R g+w c_app/tmp c_app/log
+
+sudo chown $CGUSER:$CGUSER c_app/backups
+chmod -R go-rwX,o+rw c_app/backups
+
+sudo chown -R $CGUSER:$CGSERV httpdocs
+chmod -R g-wx,o-rwx,u+rwX,g+rX httpdocs
+
+
+echo -e "\n\n  READY. Enjoy :)  \n\n"
 
