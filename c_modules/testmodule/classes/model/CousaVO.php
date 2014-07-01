51d64c9959dcfb9a63d238bf7ae8c4060eae4901
Jose M. Porto <jmporto@innoto.es>
2014-07-01 15:05:54 +0200

    Cambios de estilo y control de acceso a Devel

--- c_modules/testmodule/classes/model/CousaVO.php
+++ c_modules/testmodule/classes/model/CousaVO.php
@@ -5,43 +5,43 @@ testmodule::load('model/ComplementoVO');
 
 
 class CousaVO extends VO
-{	
-	static $tableName = 'cousa';
-	static $cols = array(
-		'id' => array(
-			'type'=>'INT', 
-			'primarykey'=>true,
-			'autoincrement'=>true
-		),
-		'name' => array(
-			'desc' => 'Nome', 
-			'type'=>'VARCHAR', 
-			'size'=>30
-		),
-		'fingers'=> array(
-			'desc' => 'Número de dedos', 
-			'type'=>'INT'
-		),
-		'hobby'=> array(
-			'desc' => 'Afición', 
-			'type'=>'VARCHAR', 
-			'size'=>30
-		),
-		'complemento' => array(
-      'desc' => 'Complemento', 
-      'type'=>'FOREIGN', 
+{
+  static $tableName = 'cousa';
+  static $cols = array(
+    'id' => array(
+      'type'=>'INT',
+      'primarykey'=>true,
+      'autoincrement'=>true
+    ),
+    'name' => array(
+      'desc' => 'Nome',
+      'type'=>'VARCHAR',
+      'size'=>30
+    ),
+    'fingers'=> array(
+      'desc' => 'Número de dedos',
+      'type'=>'INT'
+    ),
+    'hobby'=> array(
+      'desc' => 'Afición',
+      'type'=>'VARCHAR',
+      'size'=>30
+    ),
+    'complemento' => array(
+      'desc' => 'Complemento',
+      'type'=>'FOREIGN',
       'vo' => 'ComplementoVO',
       'vo_key' => 'id'
-		)
-	);
+    )
+  );
 
 
 
-	function __construct($datarray = array())
-	{
-		parent::__construct($datarray);
-	}
-	
+  function __construct($datarray = array())
+  {
+    parent::__construct($datarray);
+  }
+
 
 
 }
--- c_packages/base_app/c_app/conf/setup.php
+++ c_packages/base_app/c_app/conf/setup.php
@@ -1,9 +1,11 @@
 <?php
 
-if( is_devel_server() ) {
+if( develEnviroment() ) {
+  define( 'IS_DEVEL_ENV', true );
   require_once('setup.dev.php');
 }
 else {
+  define( 'IS_DEVEL_ENV', false );
   require_once('setup.final.php');
 }
 
@@ -13,23 +15,23 @@ else {
 * Calcula si estamos en el entorno de desarrollo o produccion
 * @return bool
 */
-function is_devel_server() {
-  $develEnviroment = false;
+function develEnviroment() {
+  $develEnv = false;
 
   if( isset( $_SERVER['REMOTE_ADDR'] ) ) {
     if ( strpos( $_SERVER['REMOTE_ADDR'], '10.77.' ) === 0 ||
       strpos( $_SERVER['REMOTE_ADDR'], '127.0.' ) === 0
     ) {
-      $develEnviroment = true;
+      $develEnv = true;
     }
   }
   else {
     $last_line = exec('ip addr show | grep 10.72.');
     if ( strpos( $last_line, '10.77.' ) !== false ) {
-      $develEnviroment = true;
+      $develEnv = true;
     }
   }
 
-  return $develEnviroment;
+  return $develEnv;
 }
 
