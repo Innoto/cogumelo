ec9e8acee9722cae9e24340df9159d8c64de6b54
pablinhob <pablinho.blanco@gmail.com>
2014-07-02 09:28:42 +0200

    cambiados nomes a nova notación

--- c_classes/c_model/DAO.php
+++ c_classes/c_model/DAO.php
@@ -1,23 +1,4 @@
 <?php
-/*
-Cogumelo v1.0 - Innoto S.L.
-Copyright (C) 2013 Innoto Gestión para el Desarrollo Social S.L. <mapinfo@map-experience.com>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-*/
 
 
 //
@@ -26,27 +7,27 @@ USA.
 
 class DAO
 {
-	public static function factory($entity, $module)
-	{
-		
+  public static function factory($entity, $module)
+  {
+    
 
-		$classpath = 'model/'. DB_ENGINE . '/'. ucfirst(DB_ENGINE) .$entity.'DAO';
+    $classPath = 'model/'. DB_ENGINE . '/'. ucfirst(DB_ENGINE) .$entity.'DAO';
 
-		// check if entity is in module or is in main project
-		if($module) {
-			eval($module.'::load("'. $classpath .'");');
-		}
-		else
-		{
-			Cogumelo::load($classpath);
-		}
+    // check if entity is in module or is in main project
+    if($module) {
+      eval($module.'::load("'. $classPath .'");');
+    }
+    else
+    {
+      Cogumelo::load($classPath);
+    }
 
 
 
-		$dao_obj = ucfirst(DB_ENGINE).$entity;
+    $daoObj = ucfirst(DB_ENGINE).$entity;
 
-		eval('$dao_obj_return = new '.$dao_obj.'DAO();');
-		return $dao_obj_return;
-	}
+    eval('$daoObjReturn = new '.$daoObj.'DAO();');
+    return $daoObjReturn;
+  }
 }
 ?>
--- c_classes/c_model/VO.php
+++ c_classes/c_model/VO.php
@@ -4,86 +4,103 @@
 Class VO
 {
 
-	var $attributes=array();
+  var $attributes = array();
 
 
-	function __construct(array $datarray){
+  function __construct(array $datarray){
 
-		// Common developer errors
-		if(!isset($this::$tableName)){
-			Cogumelo::error('all VO Must have declared an $this::$tableName');
-			return false;
-		}
-		if(!isset($this::$cols)){
-			Cogumelo::error($this::$tableName.'VO Must have an self::$cols array (See Cogumelo documentation)');
-			return false;
-		}
-		if(!$this->getFirstPrimarykeyId()){
-			Cogumelo::error($this::$tableName.'VO Must be declared at least one primary key in $this::$cools array (See Cogumelo documentation)');
-			return false;
-		}
+    // Common developer errors
+    if(!isset($this::$tableName)){
+      Cogumelo::error('all VO Must have declared an $this::$tableName');
+      return false;
+    }
+    if(!isset($this::$cols)){
+      Cogumelo::error($this::$tableName.'VO Must have an self::$cols array (See Cogumelo documentation)');
+      return false;
+    }
+    if(!$this->getFirstPrimarykeyId()){
+      Cogumelo::error($this::$tableName.'VO Must be declared at least one primary key in $this::$cools array (See Cogumelo documentation)');
+      return false;
+    }
 
-		$this->setVarlist($datarray);
-	}
+    $this->setVarlist($datarray);
+  }
 
-	// set variable list (initializes entity)
-	function setVarlist(array $datarray) 
-	{
-		// rest of variables
-		foreach($datarray as $datakey=>$data) {
-				$this->setter($datakey, $data, true);
-		}
-	}
-	
+  // set variable list (initializes entity)
+  function setVarlist(array $datarray) 
+  {
+    // rest of variables
+    foreach($datarray as $datakey=>$data) {
+        $this->setter($datakey, $data, true);
+    }
+  }
+  
 
-	function getFirstPrimarykeyId() {
+  function getFirstPrimarykeyId() {
 
-		foreach($this::$cols as $cid => $col) {
-			if(array_key_exists('primarykey', $this::$cols[$cid])){
-				if($this::$cols[$cid]['primarykey'] == true){
-					return $cid;
-				}
-			}
-		}
+    foreach($this::$cols as $cid => $col) {
+      if(array_key_exists('primarykey', $this::$cols[$cid])){
+        if($this::$cols[$cid]['primarykey'] == true){
+          return $cid;
+        }
+      }
+    }
 
-		return false;
-	}
-	
-	function getTableName() {
-		return $this->tableName;
-	}
+    return false;
+  }
+  
+  function getTableName() {
+    return $this->tableName;
+  }
 
-	// set a attribute. If exist a manual method use it
-	function setter($setterkey, $value = false, $ignore_nonexistent_key = false)
-	{
-		if( in_array($setterkey, array_keys($this::$cols)) ){
-			$this->attributes[$setterkey] = $value;
-		}
-		else{
-			if(!$ignore_nonexistent_key)
-				Cogumelo::error("key '". $setterkey ."' doesn't exist in VO::". $this::$tableName);
-		}
-			
-	}
+  // set a attribute. If exist a manual method use it
+  function setter($setterkey, $value = false, $ignore_nonexistent_key = false)
+  {
+    if( in_array($setterkey, array_keys($this::$cols)) ){
+      $this->attributes[$setterkey] = $value;
+    }
+    else{
+      if(!$ignore_nonexistent_key)
+        Cogumelo::error("key '". $setterkey ."' doesn't exist in VO::". $this::$tableName);
+    }
+  }
 
-	// get a attribute. If exist a manual method use it
-	function getter($setterkey)
-	{
-		if(array_key_exists( $setterkey, $this->attributes))
-			return $this->attributes[$setterkey];
-		else
-			return null;
-	}
+  // get a attribute. If exist a manual method use it
+  function getter($setterkey)
+  {
+    if(array_key_exists( $setterkey, $this->attributes))
+      return $this->attributes[$setterkey];
+    else
+      return null;
+  }
 
+  function keysToString($resolveDependences = false) {
 
-	function toString(){
-		$str = "\n " . $keyId. ': ' .$this->getter($keyId);
-		foreach(array_keys($this->keys) as $k) {
-			$str .= "\n " . $this->keys[$k] . ': ' .$this->getter($k);
-		}
+    $keys = false;
 
-		return $str;
-	}
+    if( $resolveDependences ) {
+
+    }
+    else {
+      $comma = ' ';
+      foreach( array_keys($this->cols) as $k ) {
+        $keys .= $comma.$k;
+        $comma = ', ';
+      }
+    }
+
+    return $keys;
+    
+  }
+
+  function toString(){
+    $str = "\n " . $keyId. ': ' .$this->getter($keyId);
+    foreach(array_keys($this->cools) as $k) {
+      $str .= "\n " . $this->cools[$k] . ': ' .$this->getter($k);
+    }
+
+    return $str;
+  }
 
 
 
