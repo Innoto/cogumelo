c500e66c28eb1f1882b16230eec87c540b9c4180
pablinhob <pablinho.blanco@gmail.com>
2014-08-19 14:14:27 +0200

    Configuración + script de busqueda en módulos de adrian

--- c_packages/base_app/c_app/conf/setup.dev.php
+++ c_packages/base_app/c_app/conf/setup.dev.php
@@ -69,8 +69,8 @@ define('SMARTY_CACHE',  SITE_PATH.'tmp/cache');
 //
 
 define('MEDIASERVER_MINIMIFY_FILES', false); // minimify js and css files
-define('MEDIASERVER_TMP_CACHE_PATH', SITE_PATH.'tmp/mediaserver');
-define('MEDIASERVER_FINAL_CACHE_PATH', SITE_PATH.'httpdocs/mediaCache');
+define('MEDIASERVER_TMP_CACHE_PATH', SITE_PATH.'tmp/mediaCache');
+define('MEDIASERVER_FINAL_CACHE_PATH', SITE_PATH.'../httpdocs/mediaCache');
 define('MEDIASERVER_CACHE_TIME', 2000); // in ms (when devel mode is enabled ignore cache times)
 define('MEDIASERVER_COMPILE_LESS', true); 
 
