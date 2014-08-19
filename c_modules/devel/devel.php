96c8c14427f99ff3c31835df3293b40532c1de26
arodriguez <adrian.kewelinho@gmail.com>
2014-08-19 14:15:03 +0200

    .

--- c_modules/devel/devel.php
+++ c_modules/devel/devel.php
@@ -11,18 +11,19 @@ class devel extends Module
   public $version = "2.0";
   public $dependencies = array(
    array(
-     "id" =>"jquery",
+     "id" => "jquery",
      "params" => array("jquery#1.*"),
      "installer" => "bower",
      "load" => array("jquery.js")
    ),
    array(
-     "id" =>"jquery-ui",
+     "id" => "jquery-ui",
      "params" => array("jquery-ui"),
      "installer" => "bower",
      "load" => array("jquery-ui.js", "jquery-ui.css")
    ),
    array(
+     "id" => "bootstrap",
      "params" => array("bootstrap"),
      "installer" => "bower",
      "load" => array("bootstrap.css")
