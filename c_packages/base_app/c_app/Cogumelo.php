68745cf02c5649f9ad1ca03d4140081e6913f941
arodriguez <adrian.kewelinho@gmail.com>
2014-09-04 13:56:32 +0200

    .

--- c_packages/base_app/c_app/Cogumelo.php
+++ c_packages/base_app/c_app/Cogumelo.php
@@ -29,14 +29,9 @@ class Cogumelo extends CogumeloClass
     $this->addUrlPatterns( '#^loadform#', 'view:Forms::loadForm' );
     $this->addUrlPatterns( '#^ajax_file_upload_parser$#', 'view:Forms::ajaxUpload' );
     $this->addUrlPatterns( '#^omeuphp#', 'view:Forms::phpinfo' );
-
     $this->addUrlPatterns( '#^actionform#', 'view:FormAction::actionForm' );
-
-
-
     $this->addUrlPatterns( '#^getobj$#', 'view:Adminview::getobj' );
     $this->addUrlPatterns( '#^setobj$#', 'view:Adminview::setobj' );
-
     $this->addUrlPatterns( '#^404$#', 'view:MasterView::page404' );
 
     $this->addUrlPatterns( '#^$#', 'view:MasterView::master' ); // App home url
