1f692eac343423cd4419dd2a4922f175a7c44f0f
Jose M. Porto <jmporto@trasno>
2014-09-22 18:19:46 +0200

    Soporte para inputs select múltiple, checkbox o radio sin valor

--- c_packages/base_app/httpdocs/js/jquery-validation/inArray.js
+++ c_packages/base_app/httpdocs/js/jquery-validation/inArray.js
@@ -1,7 +1,8 @@
 $.validator.addMethod(
   "inArray",
   function( value, element, param ) {
-    if( Object.prototype.toString.call( value ) === '[object Array]' ) {
+    valueIsArray = Object.prototype.toString.call( value ) === '[object Array]';
+    if( value===undefined || value===false || value===null || valueIsArray ) {
       valueText = false;
     }
     else {
@@ -10,11 +11,11 @@ $.validator.addMethod(
     // No usamos .indexOf ni inArray porque cosideran 1 != "1" y no me interesa
     for(var i=0; i<param.length; i++) {
       if( ( valueText!==false && param[i].toString() === valueText ) ||
-          ( valueText===false && value.indexOf( param[i].toString() ) !== -1 ) ) {
+          ( valueIsArray && value.indexOf( param[i].toString() ) !== -1 ) ) {
         return true;
       }
     }
-    return ( value==='' && this.optional( element ) );
+    return ( (value===undefined || value===false || value===null) && this.optional( element ) );
   },
   $.validator.format("Please enter a valid value")
 );
