09cb7e1675df963aa04672e499597d0d4e05c31f
Jose M. Porto <jmporto@trasno>
2014-09-23 12:22:40 +0200

    Los input sin valor se codifican como FALSE

--- c_packages/base_app/httpdocs/js/jquery.serializeFormToObject.js
+++ c_packages/base_app/httpdocs/js/jquery.serializeFormToObject.js
@@ -5,16 +5,25 @@ $.fn.serializeFormToObject = function () {
   var o = {};
   var a = this.serializeArray();
 
-  $.each(a, function () {
-    if (o[this.name] !== undefined) {
-      if (!o[this.name].push) {
+  $.each( a, function () {
+    if( o[this.name] !== undefined ) {
+      if( !o[this.name].push ) {
         o[this.name] = [o[this.name]];
       }
-      o[this.name].push(this.value || '');
-    } else {
+      o[this.name].push( this.value || '' );
+    }
+    else {
       o[this.name] = this.value || '';
     }
   });
+
+  this.find(':input').each(
+    function(i, elem) {
+      console.log( elem.name );
+      if( o[elem.name] === undefined ) {
+        o[elem.name] = false;
+      }
+    });
 
   console.log( o );
 
