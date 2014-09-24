ef7fe61c090978bfbb4480ab762f994d980d29fb
pablinhob <pablinho.blanco@gmail.com>
2014-09-24 11:20:25 +0200

    clase de dependencias

---
+++ c_classes/c_controller/DependencesController.php
@@ -0,0 +1,143 @@
+<?php
+
+Cogumelo::load('c_controller/ModuleController');
+
+
+Class DependencesController {
+
+
+  function installDependences()
+  {
+    global $allDependencesBower; 
+    $allDependencesBower = array();
+    global $allDependencesComposer;
+    $allDependencesComposer = array();
+
+    $this->loadDependences();    
+    //Descomentar para ver las depen a instalar 
+    //error_log( print_r( "ALLBOWER", true));
+    //error_log( print_r( $allDependencesBower, true));
+    //error_log( print_r( "ALLCOMPOSER", true));
+    //error_log( print_r( $allDependencesComposer, true));
+
+    $this->installDependencesBower($allDependencesBower);
+    $this->installDependencesComposer($allDependencesComposer);    
+  }
+  
+  function loadDependences(){
+
+    $moduleControl = new ModuleController(false, true);
+
+    //Cargamos las dependencias de los modulos
+    global $C_ENABLED_MODULES;    
+    foreach ( $C_ENABLED_MODULES as $mod ){      
+      $modUrl = ModuleController::getRealFilePath( $mod.".php" , $mod );
+      require_once($modUrl);
+      eval('class extClass'. $mod .' extends '.$mod. '{}');
+      eval('$objMod'.$mod.' = new extClass'.$mod.'();');
+      eval('$dependences = $objMod'.$mod.'->dependences;');
+      
+      $this->pushDependences($dependences);
+    }    
+
+    //Cargamos las dependencias de Base App (externas a los modulos).
+    $this->pushDependences($_C->dependences);
+    
+  }
+  
+  
+  
+  function pushDependences($dependences)
+  {
+    //Hacemos una lista de las dependecias de todos los modulos
+    foreach ( $dependences as $dependence ){     
+
+      //Diferenciamos entre instaladores
+      switch($dependence['installer']){
+        case "composer":
+          $this->pushDependencesComposer($dependence);
+        break;
+        case "bower":
+          $this->pushDependencesBower($dependence);    
+        break;
+      }
+    }   // end foreach             
+
+    
+  }
+  
+  
+  function pushDependencesComposer($dependence)
+  {
+    global $allDependencesComposer;
+    if(!array_key_exists($dependence['id'], $allDependencesComposer)){
+      $allDependencesComposer[$dependence['id']] = array($dependence['params']);
+    }
+    else{
+      $diffAllDepend = array_diff($dependence['params'] , $allDependencesComposer[$dependence['id']][0]); 
+
+      if(!empty($diffAllDepend)){
+        array_push($allDependencesComposer[$dependence['id']], array_diff($dependence['params'] , $allDependencesComposer[$dependence['id']][0])  );
+      }          
+    }
+  }
+  
+  function pushDependencesBower($dependence)
+  {
+    global $allDependencesBower;
+    if(!array_key_exists($dependence['id'], $allDependencesBower)){
+      $allDependencesBower[$dependence['id']] = array($dependence['params']);
+    }
+    else{
+      $diffAllDepend = array_diff($dependence['params'] , $allDependencesBower[$dependence['id']][0]); 
+
+      if(!empty($diffAllDepend)){
+        array_push($allDependencesBower[$dependence['id']], array_diff($dependence['params'] , $allDependencesBower[$dependence['id']][0])  );
+      }          
+    }
+  }
+  
+  function installDependencesBower($dependences)
+  {
+    //Instala las dependecias con Bower
+    
+    foreach( $dependences as $depKey => $dep ){
+      foreach( $dep as $params ){                
+        if(count($params) > 1){
+          $allparam = "";
+          foreach( $params as $p ){
+            $allparam = $allparam." ".$p;
+          } // end foreach
+          echo("Exec ... bower install ".$depKey."=".$allparam." --save\n");
+          exec('bower install '.$depKey.'='.$allparam.' --save');          
+        }
+        else{
+          echo("Exec ... bower install ".$depKey."=".$params[0]." --save\n");
+          exec('bower install '.$depKey.'='.$params[0].' --save');
+
+        }
+      }       // end foreach
+    }   // end foreach
+    
+  }
+
+  function installDependencesComposer($dependences)
+  {
+    
+    $finalArrayDep = array("require" => array(), "config" => array("vendor-dir" => "httpdocs/vendorServer"));
+    foreach( $dependences as $depKey => $dep ){
+      foreach( $dep as $params ){   
+        $finalArrayDep['require'][$params[0]] = $params[1];
+      }
+    }
+    $jsonencoded = json_encode($finalArrayDep);
+    $fh = fopen("composer.json", 'w');
+      fwrite($fh, $jsonencoded);
+    fclose($fh);
+    echo("Exec ... php composer.phar update\n\n");          
+    exec('php composer.phar update');
+    echo("If the folder does not appear vendorServer dependencies run 'php composer.phar update' or 'composer update' and resolves conflicts.\n");
+    
+  }
+
+}
