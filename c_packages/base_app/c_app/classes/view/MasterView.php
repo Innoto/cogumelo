b809640c4b4a4222b65d7e54ffdadac708f6a303
kewelinho <adrian.kewelinho@gmail.com>
2014-10-09 13:20:38 +0200

    .

--- c_packages/base_app/c_app/classes/view/MasterView.php
+++ c_packages/base_app/c_app/classes/view/MasterView.php
@@ -35,13 +35,7 @@ class MasterView extends View
 
   function common() {
     $this->template->setTpl('default.tpl');
-
-    //$this->template->addJs('vendorLib/jQuery.js' , 'client');
-    //$this->template->addJs('vendorLib/less.js', 'client');
-
     $this->template->addClientStyles('styles/client.css', 'client');
-
-
   }
 
   function page404() {
