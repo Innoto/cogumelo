831b9c28cba3dcb4a00ceea474c84f099062fb46
kewelinho <adrian.kewelinho@gmail.com>
2014-11-05 17:37:25 +0100

    .

--- c_packages/base_app/c_app/conf/setup.dev.php
+++ c_packages/base_app/c_app/conf/setup.dev.php
@@ -80,7 +80,7 @@ define('MEDIASERVER_COMPILE_LESS', false);
 global $C_ENABLED_MODULES;
 global $C_INDEX_MODULES;
 
-$C_ENABLED_MODULES = array('mediaserver', 'i18nGetLang', 'testmodule', 'common', 'devel', 'form', 'table', 'user');
+$C_ENABLED_MODULES = array('mediaserver', 'i18nGetLang', 'common', 'devel', 'form', 'table', 'user');
 $C_INDEX_MODULES  = array('mediaserver', 'i18nGetLang', 'user', 'devel'); 			// before c_app/Cogumelo.php execution
 
 
