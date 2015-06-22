28cf2b39ccc841d2fc9256d5f9fddd3d1298987e
Jose M. Porto <jmporto@innoto.es>
2015-06-22 13:47:08 +0200

    Añado TINYINT y SMALLINT a los modelos

--- coreModules/devel/classes/model/mysql/MysqlDevelDBDAO.php
+++ coreModules/devel/classes/model/mysql/MysqlDevelDBDAO.php
@@ -10,6 +10,8 @@ class MysqlDevelDBDAO extends MysqlDAO
 {
 
   static $conversion_types = array(
+    'TINYINT' => 'TINYINT',
+    'SMALLINT' => 'SMALLINT',
     'INT' => 'INT',
     'BIGINT' => 'BIGINT',
     'FLOAT' => 'FLOAT',
@@ -32,25 +34,25 @@ class MysqlDevelDBDAO extends MysqlDAO
     'GEOMETRYCOLLECTION' => 'GEOMETRYCOLLECTION'
   );
 
-  function createSchemaDB($connection){
+  public function createSchemaDB( $connection ) {
 
     $resultado =  array();
 
     $strSQL0 = "DROP DATABASE IF EXISTS ". DB_NAME ;
     $strSQL1 = "CREATE DATABASE ". DB_NAME ;
     $strSQL2 = "GRANT ".
-            "SELECT, ".
-            "INSERT, ".
-            "UPDATE, ".
-            "DELETE, ".
-            "INDEX, ".
-            "LOCK TABLES, ".
-            "CREATE VIEW, ".
-            "CREATE, ".
-            "DROP, ".
-            "SHOW VIEW ".
-          "ON ". DB_NAME .".* ".
-          "TO '". DB_USER ."'@'localhost' IDENTIFIED BY '". DB_PASSWORD ."' ";
+        "SELECT, ".
+        "INSERT, ".
+        "UPDATE, ".
+        "DELETE, ".
+        "INDEX, ".
+        "LOCK TABLES, ".
+        "CREATE VIEW, ".
+        "CREATE, ".
+        "DROP, ".
+        "SHOW VIEW ".
+      "ON ". DB_NAME .".* ".
+      "TO '". DB_USER ."'@'localhost' IDENTIFIED BY '". DB_PASSWORD ."' ";
 
     $resultado[] = $this->execSQL($connection, $strSQL0, array() );
     $resultado[] = $this->execSQL($connection, $strSQL1, array() );
@@ -61,18 +63,19 @@ class MysqlDevelDBDAO extends MysqlDAO
 
 
 
-  function dropTable($connection, $vo_name) {
+  public function dropTable( $connection, $vo_name ) {
     $this->execSQL($connection, $this->getDropSQL($connection, $vo_name) , array() );
   }
 
-  function createTable($connection, $vo_name) {
+  public function createTable( $connection, $vo_name ) {
     $strSQL = $this->getTableSQL($connection, $vo_name);
     $this->execSQL($connection, $strSQL, array() );
   }
-  function insertTableValues($connection, $vo_name){
+
+  public function insertTableValues( $connection, $vo_name ){
     $res = $this->getInsertTableSQL($connection, $vo_name);
     if(!empty($res)) {
-      foreach ($res as $resKey => $resValue) {
+      foreach( $res as $resKey => $resValue ) {
         $this->execSQL($connection, $resValue['strSQL'], $resValue['valuesSQL']);
       }
     }
@@ -81,7 +84,7 @@ class MysqlDevelDBDAO extends MysqlDAO
 
   // Sql generation methods
 
-  function getDropSQL($connection, $vo_name, $vo_route = false ) {
+  public function getDropSQL( $connection, $vo_name, $vo_route = false ) {
     $vo= new $vo_name();
 
     $strSQL = $this->getTableSQL($connection, $vo_name, $vo_route);
@@ -89,20 +92,20 @@ class MysqlDevelDBDAO extends MysqlDAO
 
   }
 
-  function getTableSQL($connection, $vo_name, $vo_route = false){
+  public function getTableSQL( $connection, $vo_name, $vo_route = false ){
     $VO = new $vo_name();
 
     $primarykeys = array();
     $uniques = array();
     $lines = array();
 
-    foreach($VO::$cols as $colkey => $col) {
+    foreach( $VO::$cols as $colkey => $col ) {
 
       if( isset( $col['multilang'] ) && $col['multilang'] == true &&  $col['type'] != 'FOREIGN'  ) {
 
         global $LANG_AVAILABLE;
 
-        foreach ( array_keys($LANG_AVAILABLE) as $langKey) {
+        foreach( array_keys($LANG_AVAILABLE) as $langKey ) {
 
           $retMLC = $this->multilangCols( $colkey.'_'.$langKey, $col,  $primarykeys, $uniques, $lines );
           $primarykeys = $retMLC['primarykeys'];
@@ -132,52 +135,52 @@ class MysqlDevelDBDAO extends MysqlDAO
 
 
 
-  function multilangCols( $colkey, $col,  $primarykeys, $uniques, $lines) {
+  public function multilangCols( $colkey, $col, $primarykeys, $uniques, $lines ) {
 
-      $extrapkey = "";
-      $type = "";
-      $size = "";
+    $extrapkey = "";
+    $type = "";
+    $size = "";
 
-      // is primary key
-      if( array_key_exists('primarykey', $col )){
-        $primarykeys[] = $colkey;
-      }
+    // is primary key
+    if( array_key_exists('primarykey', $col ) ) {
+      $primarykeys[] = $colkey;
+    }
 
-      // is autoincrement
-      if(  array_key_exists('autoincrement', $col ) ){
-        $extrapkey=' NOT NULL auto_increment ';
-      }
+    // is autoincrement
+    if(  array_key_exists('autoincrement', $col ) ){
+      $extrapkey=' NOT NULL auto_increment ';
+    }
 
-      // is unique
-      if(  array_key_exists('unique', $col ) ){
-        $uniques[] = $colkey;
-      }
+    // is unique
+    if(  array_key_exists('unique', $col ) ){
+      $uniques[] = $colkey;
+    }
 
 
-      if( $col['type'] == "FOREIGN" ) { // is a foreign key
-        eval( '$foreign_col = '.$col['vo'].'::$cols[\''.$col['key'].'\'];' );
-        $type = $this::$conversion_types[$foreign_col['type']].$size;
-      }
-      else {
-        $size = (array_key_exists('size', $col))? '('.$col['size'].') ': '';
-        $type = $this::$conversion_types[$col['type']].$size;
-      }
+    if( $col['type'] == "FOREIGN" ) { // is a foreign key
+      eval( '$foreign_col = '.$col['vo'].'::$cols[\''.$col['key'].'\'];' );
+      $type = $this::$conversion_types[$foreign_col['type']].$size;
+    }
+    else {
+      $size = (array_key_exists('size', $col))? '('.$col['size'].') ': '';
+      $type = $this::$conversion_types[$col['type']].$size;
+    }
 
-      $lines[] = '`'.$colkey.'` '.$type.$extrapkey;
+    $lines[] = '`'.$colkey.'` '.$type.$extrapkey;
 
 
 
     return array(
-        'primarykeys' => $primarykeys,
-        'uniques' => $uniques,
-        'lines' => $lines
-      );
+      'primarykeys' => $primarykeys,
+      'uniques' => $uniques,
+      'lines' => $lines
+    );
   }
 
 
 
 
-  function getInsertTableSQL($connection, $vo_name, $vo_route = false ) {
+  public function getInsertTableSQL($connection, $vo_name, $vo_route = false ) {
     $VO = new $vo_name();
     $primarykey = $VO->getFirstPrimarykeyId();
     $valuesSQL = array();
