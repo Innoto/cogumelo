b981688095579a7c748e7ae927f469a0929bec64
Jose M. Porto <jmporto@innoto.es>
2015-08-27 10:04:19 +0200

    Reorganizando los metodos de carga de datos en Less para adaptarlos al compilador

--- coreModules/mediaserver/classes/controller/LessController.php
+++ coreModules/mediaserver/classes/controller/LessController.php
@@ -5,17 +5,23 @@ class LessController {
 
   var $less = false;
 
-  function __construct() {
-    $this->less = new lessc();
+  public function __construct() {
+    // $this->less = new lessc();
   }
 
-  /*
-  * Compile less file
-  * @return string : the path of compiled file
-  * @var string $path: less file to compile
-  */
-  function compile( $lessFilePath, $resultFilePath , $moduleName ) {
+  /**
+   * Compile less file
+   *
+   * @param string $lessFilePath: less file to compile
+   *
+   * @return boolean
+   */
+  public function compile( $lessFilePath, $resultFilePath, $moduleName ) {
     $ret = true;
+
+    if( $this->less === false ) {
+      $this->less = new lessc();
+    }
 
     // generate less caches
     $lessTmpDir = CacheUtilsController::prepareLessTmpdir();
@@ -24,9 +30,12 @@ class LessController {
     $this->less->setImportDir( $lessTmpDir );
 
     // set less variables (Defined in setup)
+    /*
     global $MEDIASERVER_LESS_CONSTANTS;
-
     $this->less->setVariables( $MEDIASERVER_LESS_CONSTANTS ) ;
+    */
+    $this->less->setVariables( $this->getLessVarsFromSetup() ) ;
+
 
     try {
       $this->less->checkedCompile( $lessTmpDir.$moduleName.'/classes/view/templates/'.$lessFilePath, $resultFilePath );
@@ -38,4 +47,118 @@ class LessController {
     return $ret;
   }
 
+
+  public function getLessVarsFromSetup() {
+    $lessVars = array();
+
+    global $MEDIASERVER_LESS_GLOBALS, $MEDIASERVER_LESS_CONSTANTS;
+
+    if( is_array( $MEDIASERVER_LESS_GLOBALS ) && count( $MEDIASERVER_LESS_GLOBALS ) > 0 ) {
+      foreach( $MEDIASERVER_LESS_GLOBALS as $globalKey ) {
+        if( isset( $GLOBALS[ $globalKey ] ) ) {
+          $lessValue = $this->valueToLess( $GLOBALS[ $globalKey ] );
+          if( $lessValue !== null ) {
+            $lessVars[ 'GLOBAL_'.$globalKey ] = $lessValue;
+          }
+        }
+      }
+    }
+
+    if( is_array( $MEDIASERVER_LESS_CONSTANTS ) && count( $MEDIASERVER_LESS_CONSTANTS ) > 0 ) {
+      foreach( $MEDIASERVER_LESS_CONSTANTS as $name => $value ) {
+        //$lessContent .= '@'.$name.' : "'.$value.'";'."\n";
+        $lessValue = $this->valueToLess( $value );
+        if( $lessValue !== null ) {
+          $lessVars[ $name ] = $lessValue;
+        }
+      }
+    }
+
+    return ( count( $lessVars ) > 0 ) ? $lessVars : false;
+  }
+
+
+  public function getLessFromSetup() {
+    $lessContent = '/* COGUMELO SETUP CONSTANTS */'."\n";
+
+    if( $lessVars = $this->getLessVarsFromSetup() ) {
+      foreach( $lessVars as $name => $lessValue ) {
+        $lessContent .= '@'.$name.' : '.$lessValue.';'."\n";
+      }
+    }
+
+    $lessContent .= '/* END SETUP CONSTANTS */'."\n";
+
+    return $lessContent;
+  }
+
+
+  public function valueToLess( $value ) {
+    $lessValue = null;
+
+    switch( gettype( $value ) ) {
+      case 'integer':
+      case 'double':
+      case 'float':
+        $lessValue = $value;
+        break;
+
+      case 'boolean':
+        $lessValue = ($value) ? 'true' : 'false';
+        break;
+
+      case 'array':
+        $arrAssoc = $this->isAssoc( $value );
+        $arrResult = array();
+        if( !$arrAssoc ) { // No conocemos como crear arrays asociativos en Less
+          foreach( $value as $valueKey => $valueData ) {
+            if( is_array( $valueData ) ) { // No conocemos como crear arrays anidados en Less
+              error_log( 'ConfConstantsView: No conocemos como crear arrays asociativos en Less' );
+              $arrResult = null;
+              break;
+            }
+            $valueDataLess = $this->valueToLess( $valueData );
+            if( $valueDataLess === null ) {
+              $arrResult = null;
+              break;
+            }
+            $arrResult[] = $valueDataLess;
+          }
+        }
+        else {
+          $arrResult = null;
+          error_log( 'ConfConstantsView: No conocemos como crear arrays asociativos en Less' );
+        }
+
+        if( $arrResult !== null ) {
+          $lessValue = implode( ', ', $arrResult );
+        }
+        break;
+
+      case 'NULL':
+        $lessValue = 'null';
+        break;
+
+      case 'object':
+      case 'resource':
+      case 'unknown type':
+        break;
+
+      case 'string':
+      default:
+        $lessValue = '\''.addslashes( $value ).'\'';
+        break;
+    }
+
+    if( $lessValue === null ) {
+      error_log( 'ConfConstantsView valueToLess: No hemos convertido este valor de tipo '.gettype( $value ) );
+      error_log( print_r( $value, true ) );
+    }
+    return $lessValue;
+  }
+
+
+  private function isAssoc( $arr ) {
+    return( array_keys($arr) !== range(0, count($arr) - 1) );
+  }
 }
--- coreModules/mediaserver/classes/view/ConfConstantsView.php
+++ coreModules/mediaserver/classes/view/ConfConstantsView.php
@@ -1,40 +1,18 @@
 <?php
+mediaserver::load('controller/LessController.php');
+
 
 class ConfConstantsView {
-
 
   public function __construct() {
 
   }
 
 
-  public function less(){
-    $lessContent = '/* COGUMELO SETUP CONSTANTS */'."\n";
+  public function less() {
+    $lessController = new LessController();
 
-    global $MEDIASERVER_LESS_GLOBALS, $MEDIASERVER_LESS_CONSTANTS;
-
-    if( is_array( $MEDIASERVER_LESS_GLOBALS ) && count( $MEDIASERVER_LESS_GLOBALS ) > 0 ) {
-      foreach( $MEDIASERVER_LESS_GLOBALS as $globalKey ) {
-        if( isset( $GLOBALS[ $globalKey ] ) ) {
-          $lessValue = $this->valueToLess( $GLOBALS[ $globalKey ] );
-          if( $lessValue !== null ) {
-            $lessContent .= '@GLOBAL_'.$globalKey.' : '.$lessValue.';'."\n";
-          }
-        }
-      }
-    }
-
-    if( is_array( $MEDIASERVER_LESS_CONSTANTS ) && count( $MEDIASERVER_LESS_CONSTANTS ) > 0 ) {
-      foreach( $MEDIASERVER_LESS_CONSTANTS as $name => $value ) {
-        //$lessContent .= '@'.$name.' : "'.$value.'";'."\n";
-        $lessValue = $this->valueToLess( $value );
-        if( $lessValue !== null ) {
-          $lessContent .= '@'.$name.' : '.$lessValue.';'."\n";
-        }
-      }
-    }
-    $lessContent .= '/* END SETUP CONSTANTS */'."\n";
-
+    $lessContent = $lessController->getLessFromSetup();
 
     header('Content-Type: text/less');
     echo $lessContent;
@@ -72,70 +50,6 @@ class ConfConstantsView {
     echo $jsContent;
   }
 
-
-  private function valueToLess( $value ) {
-    $lessValue = null;
-
-    switch( gettype( $value ) ) {
-      case 'integer':
-      case 'double':
-      case 'float':
-        $lessValue = $value;
-        break;
-
-      case 'boolean':
-        $lessValue = ($value) ? 'true' : 'false';
-        break;
-
-      case 'array':
-        $arrAssoc = $this->isAssoc( $value );
-        $arrResult = array();
-        if( !$arrAssoc ) { // No conocemos como crear arrays asociativos en Less
-          foreach( $value as $valueKey => $valueData ) {
-            if( is_array( $valueData ) ) { // No conocemos como crear arrays anidados en Less
-              error_log( 'ConfConstantsView: No conocemos como crear arrays asociativos en Less' );
-              $arrResult = null;
-              break;
-            }
-            $valueDataJs = $this->valueToJs( $valueData );
-            if( $valueDataJs === null ) {
-              $arrResult = null;
-              break;
-            }
-            $arrResult[] = ( $arrAssoc ) ? $valueKey.':'.$valueDataJs : $valueDataJs;
-          }
-        }
-        else {
-          $arrResult = null;
-          error_log( 'ConfConstantsView: No conocemos como crear arrays asociativos en Less' );
-        }
-
-        if( $arrResult !== null ) {
-          $lessValue = implode( ', ', $arrResult );
-        }
-        break;
-
-      case 'NULL':
-        $lessValue = 'null';
-        break;
-
-      case 'object':
-      case 'resource':
-      case 'unknown type':
-        break;
-
-      case 'string':
-      default:
-        $lessValue = '\''.addslashes( $value ).'\'';
-        break;
-    }
-
-    if( $lessValue === null ) {
-      error_log( 'ConfConstantsView valueToLess: No hemos convertido este valor de tipo '.gettype( $value ) );
-      error_log( print_r( $value, true ) );
-    }
-    return $lessValue;
-  }
 
   private function valueToJs( $value ) {
     $jsValue = null;
