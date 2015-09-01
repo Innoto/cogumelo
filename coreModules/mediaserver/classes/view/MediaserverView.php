00fc8b594894a3301ed0168569119427cd6e5cca
pablinhob <pablinho.blanco@gmail.com>
2015-09-01 11:58:23 +0200

    Solucionado bug no último cambio

--- coreModules/mediaserver/classes/view/MediaserverView.php
+++ coreModules/mediaserver/classes/view/MediaserverView.php
@@ -35,9 +35,14 @@ class MediaserverView extends View
   //load media from a module
   function module($url_path=''){
 
-    preg_match('#/(.*?)/(.*)?\??(.*)#', $url_path[1], $result);
+    //preg_match('#/(.*?)/(.*)#', $url_path[1], $result);
+    preg_match('#/(.*?)/(.*)#', $url_path[1], $result);
+
+
+    $filePath = explode('?', $result[2]);
+
     if( $result != array() ) {
-      $this->mediaserverControl->servecontent($result[2], $result[1]);
+      $this->mediaserverControl->servecontent($filePath[0], $result[1]);
       CacheUtilsController::removeLessTmpdir();
     }
     else {
