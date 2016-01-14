2ef1ae1c8ccf6886246334f37da28d08b8d05a89
Jose M. Porto <jmporto@innoto.es>
2016-01-14 13:44:37 +0100

    Reconoce idiomas con url vacia

--- coreModules/i18nGetLang/classes/view/GetLangView.php
+++ coreModules/i18nGetLang/classes/view/GetLangView.php
@@ -5,22 +5,25 @@ Cogumelo::load('coreView/View.php');
 class GetLangView extends View
 {
 
-  function __construct($base_dir){
-    parent::__construct($base_dir);
+  public function __construct( $base_dir ) {
+    parent::__construct( $base_dir );
   }
 
   /**
   * Evaluate the access conditions and report if can continue
   * @return bool : true -> Access allowed
   */
-  function accessCheck() {
+  public function accessCheck() {
     return true;
   }
 
   // load media from app
-  function setlang($url_path=''){
-    Cogumelo::load('coreController/I18nController.php');
-    I18nController::setLang($url_path);
+  public function setlang( $url_path = '' ) {
+    // error_log( 'GetLangView::setlang '.print_r( $url_path, true ) );
+
+    Cogumelo::load( 'coreController/I18nController.php' );
+    I18nController::setLang( $url_path );
+
     //echo "<br> SET Lang global variables<br><br>";
   }
 }
--- coreModules/i18nGetLang/i18nGetLang.php
+++ coreModules/i18nGetLang/i18nGetLang.php
@@ -3,36 +3,42 @@ Cogumelo::load("coreController/Module.php");
 
 class i18nGetLang extends Module
 {
-    
+
   public $name = "i18nGetLang";
   public $version = "";
   public $dependences = array();
-  
-  
-  function __construct(){
 
-  	global $LANG_AVAILABLE;
+
+  public function __construct(){
+
+    global $LANG_AVAILABLE;
 
     $i = 0;
-    if ($LANG_AVAILABLE){
-      foreach ($LANG_AVAILABLE as $l=>$lang){
+    if( $LANG_AVAILABLE ) {
+      foreach( $LANG_AVAILABLE as $l => $lang ) {
         $lang_array[$i] = $l;
         $i = $i +1;
       }
 
-      for ($j=0;$j<$i;$j++){
-        if ($j==0)
+      for( $j=0; $j<$i; $j++ ) {
+        if( $j===0 ) {
           $patron = $lang_array[$j];
-        else if ($j==$i-1)
+        }
+        else if( $j===$i-1 ) {
           $patron = $patron.'|'.$lang_array[$j];
-        else
+        }
+        else {
           $patron = $patron .'|'.$lang_array[$j];
+        }
       }
     }
-    else
+    else {
       $patron = LANG_DEFAULT;
+    }
+    // error_log( 'i18nGetLang patron: '.$patron );
 
-  	$this->addUrlPatterns( '#^('.$patron.')\/(.*)$#', 'noendview:GetLangView::setlang' );
+    $this->addUrlPatterns( '#^('.$patron.')\/(.*)$#', 'noendview:GetLangView::setlang' );
+    $this->addUrlPatterns( '#^('.$patron.')\/?()$#', 'noendview:GetLangView::setlang' );
   }
 
 }
