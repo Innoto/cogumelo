76d00aa2950fa20b5ca83747612961ea987363be
pablinhob <pablinho.blanco@gmail.com>
2016-01-28 18:21:29 +0100

    Jquery min

--- coreClasses/coreView/Template.php
+++ coreClasses/coreView/Template.php
@@ -398,7 +398,7 @@ class Template extends Smarty
       // Basic includes and includers
       $clientIncludes = "\n";
       $clientIncludes .= '<script type="text/javascript"> if( typeof $ != "undefined"){ jqueryIsLoaded = true; }else { jqueryIsLoaded = false; }</script>' . "\n";
-      $clientIncludes .= '<script type="text/javascript" src="/vendor/bower/jquery/dist/jquery.js"></script>' . "\n";
+      $clientIncludes .= '<script type="text/javascript" src="/vendor/bower/jquery/dist/jquery.min.js"></script>' . "\n";
       $clientIncludes .= '<script type="text/javascript" src="/media/module/common/js/Includes.js"></script>' . "\n";
       if( !$this->cgmMediaserverCompileLess ) {
         $clientIncludes .= '<script>less = { env: "development", async: false, fileAsync: false, poll: 1000, '.
