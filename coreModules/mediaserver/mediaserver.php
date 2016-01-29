b31f0f3ff73fa54a9a3143a469b307ed6b563318
pablinhob <pablinho.blanco@gmail.com>
2016-01-29 13:24:52 +0100

    solucionados problemas en mediaserver: urls

--- coreModules/mediaserver/mediaserver.php
+++ coreModules/mediaserver/mediaserver.php
@@ -38,8 +38,8 @@ class mediaserver extends Module
 
 
   function __construct() {
-    $this->addUrlPatterns( '#'.MEDIASERVER_FINAL_CACHE_PATH.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
-    $this->addUrlPatterns( '#'.MOD_MEDIASERVER_URL_DIR.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
+    $this->addUrlPatterns( '#^'.MEDIASERVER_FINAL_CACHE_PATH.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
+    $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
     $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'/lessConfConstants.less#', 'view:ConfConstantsView::less' );
 
     $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'/module(.*)#', 'view:MediaserverView::module' );
