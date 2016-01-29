d2c801f6a20451eb0064ee9a33f1c39b017f6851
pablinhob <pablinho.blanco@gmail.com>
2016-01-29 13:21:51 +0100

    mediaserver, repostas algunhas expresións que non tiña que cambiar

--- coreModules/mediaserver/mediaserver.php
+++ coreModules/mediaserver/mediaserver.php
@@ -40,10 +40,10 @@ class mediaserver extends Module
   function __construct() {
     $this->addUrlPatterns( '#'.MEDIASERVER_FINAL_CACHE_PATH.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
     $this->addUrlPatterns( '#'.MOD_MEDIASERVER_URL_DIR.'/jsConfConstants.js#', 'view:ConfConstantsView::javascript' );
-    $this->addUrlPatterns( '#'.MOD_MEDIASERVER_URL_DIR.'/lessConfConstants.less#', 'view:ConfConstantsView::less' );
+    $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'/lessConfConstants.less#', 'view:ConfConstantsView::less' );
 
-    $this->addUrlPatterns( '#'.MOD_MEDIASERVER_URL_DIR.'/module(.*)#', 'view:MediaserverView::module' );
-    $this->addUrlPatterns( '#'.MOD_MEDIASERVER_URL_DIR.'(.*)#', 'view:MediaserverView::application' );
+    $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'/module(.*)#', 'view:MediaserverView::module' );
+    $this->addUrlPatterns( '#^'.MOD_MEDIASERVER_URL_DIR.'(.*)#', 'view:MediaserverView::application' );
     $this->addUrlPatterns( '#(.+\/)?classes/view/templates/(.+)\.less$#', 'view:MediaserverView::onClientLess');
 
   }
