7ca49b6d6a759a45f5fffc781a00d098b685e790
mjperez <pepaps@gmail.com>
2016-02-03 17:28:43 +0100

    .

--- coreModules/i18nServer/i18nServer.php
+++ coreModules/i18nServer/i18nServer.php
@@ -6,14 +6,7 @@ class i18nServer extends Module {
   public $name = 'i18nServer';
   public $version = '1.0';
 
-  public $dependences = array(
-    array(
-      "id" => "po-to-json-master",
-      "params" => array("po-to-json-master"),
-      "installer" => "manual",
-      "includes" => array("po2json.php")
-    )
-  );
+  public $dependences = array(  );
 
   public $includesCommon = array();
 
