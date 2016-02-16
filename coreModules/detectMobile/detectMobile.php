e344e98feffd0e1406a29f01638914710f8f3448
arodriguez <adrian.kewelinho@gmail.com>
2016-02-16 17:41:10 +0100

    .

--- coreModules/detectMobile/detectMobile.php
+++ coreModules/detectMobile/detectMobile.php
@@ -14,22 +14,22 @@ class detectMobile extends Module {
       "id" =>"mobiledetect",
       "params" => array("mobiledetect/mobiledetectlib","^2.8"),
       "installer" => "composer",
-      "includes" => array("mobiledetectlib/Mobile_Detect.php")
+      "includes" => array("Mobile_Detect.php")
     )
 
   );
 
 
   public $includesCommon = array(
-    'controller/DetectMobileController.php'
   );
 
 
   public function __construct() {
-/*
-    $this->addUrlPatterns( '#^cgml-form-htmleditor-config.js#', 'view:FormConnector::customCkeditorConfig' );
-    $this->addUrlPatterns( '#^cgml-form-file-upload$#', 'view:FormConnector::fileUpload' );
-    $this->addUrlPatterns( '#^cgml-form-group-element$#', 'view:FormConnector::groupElement' );
-*/
+
+    detectMobile::autoIncludes();
+    $detect = new Mobile_Detect;
+    $isMobile = $detect->isMobile();
+    Cogumelo::setSetupValue( 'cogumelo:detectMobile:isMobile', $isMobile );
+
   }
 }
