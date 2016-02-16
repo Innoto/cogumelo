c29271c343d9303371685d31c8d2de80cf91074e
Jose M. Porto <jmporto@innoto.es>
2016-02-16 17:58:08 +0100

    Cogumelo::setSetupValue

--- coreClasses/CogumeloClass.php
+++ coreClasses/CogumeloClass.php
@@ -358,6 +358,43 @@ class CogumeloClass extends Singleton
 
 
 
+  public static function setSetupValue( $path, $value ) {
+    // error_log( 'COGUMELO::setSetupValue: '.$path );
+    global $CGMLCONF;
+
+    if( !isset( $CGMLCONF ) || !is_array( $CGMLCONF ) ) {
+      $CGMLCONF = array(
+        'cogumelo' => array()
+      );
+    }
+
+    $parts = explode( ':', $path );
+    $stack = '';
+    foreach( $parts as $key ) {
+      $valid = false;
+      $stackPrev = $stack;
+      $stack .= '[\''.$key.'\']';
+      $fai = '$valid = isset( $CGMLCONF'. $stack .');';
+      eval( $fai );
+      if( !$valid ) {
+        $fai = '$isArray = is_array( $CGMLCONF'. $stackPrev .');';
+        eval( $fai );
+        if( $isArray ) {
+          $fai = '$CGMLCONF'. $stack .' = null;';
+          eval( $fai );
+        }
+        else {
+          $fai = '$CGMLCONF'. $stackPrev .' = array( $key => null );';
+          eval( $fai );
+        }
+      }
+    }
+    $fai = '$CGMLCONF'. $stack .' = $value;';
+    eval( $fai );
+
+    return $CGMLCONF;
+  }
+
   public static function getSetupValue( $path ) {
     // error_log( 'Cogumelo::getSetupValue: '.$path );
     global $CGMLCONF;
