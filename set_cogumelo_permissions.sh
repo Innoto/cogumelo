500c52570640f3bb0373fd6b3790b150e62dbefe
raposo <jmpmato@gmail.com>
2016-04-25 13:15:52 +0200

    .

--- set_cogumelo_permissions.sh
+++ set_cogumelo_permissions.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-cd /home/proxectos/cogumelo
+cogumeloPath='/home/proxectos/cogumelo'
 
 CGUSER=$USER
 CGSERV=www-data
 
 
-sudo chown -R $CGUSER:$CGSERV *
+sudo chown -R $CGUSER:$CGSERV $cogumeloPath/*
 #chmod -R u-x,g-wx,o-rwx,u+rwX,g+rX *
-chmod -R u-x,g-wx,o-wx,u+rwX,go+rX *
-chmod 700 set_cogumelo_permissions.sh
-
-
-#chmod 700 packages/sampleApp/cogumelo
-#packages/sampleApp/cogumelo setPermissionsDevel
+chmod -R u-x,g-wx,o-wx,u+rwX,go+rX $cogumeloPath/*
+chmod 700 $cogumeloPath/set_cogumelo_permissions.sh
 
 
 echo -e "\n\n  READY. Enjoy :)  \n\n"
