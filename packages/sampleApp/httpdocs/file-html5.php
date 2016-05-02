69218229fed82db1b301918a14deae326d541bb1
Jose M. Porto <jmporto@innoto.es>
2016-05-02 19:39:14 +0200

    .

--- packages/sampleApp/httpdocs/file-html5.php
+++ packages/sampleApp/httpdocs/file-html5.php
@@ -1,4 +1,7 @@
 <html>
+<head>
+  <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+</head>
 <body>
 
 
@@ -22,86 +25,85 @@
 
 <script type="text/javascript">
 
-  // Check for the various File API support.
-  if( !(window.File && window.FileReader && window.FileList && window.Blob) ) {
-    alert('The File APIs are not fully supported in this browser.');
-  }
+// Check for the various File API support.
+if( !(window.File && window.FileReader && window.FileList && window.Blob) ) {
+  alert('The File APIs are not fully supported in this browser.');
+}
 
 
 
-  function handleFileSelect(evt) {
-    console.log( 'handleFileSelect' );
-    var files = evt.target.files; // FileList object
-    procesarFiles( files );
-  }
+function handleFileSelect(evt) {
+  console.log( 'handleFileSelect' );
+  var files = evt.target.files; // FileList object
+  procesarFiles( files );
+}
 
-  document.getElementById('inputFile').addEventListener('change', handleFileSelect, false);
-
-
-
-  function handleFileDrop(evt) {
-    console.log( 'handleFileDrop' );
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    var files = evt.dataTransfer.files; // FileList object.
-    procesarFiles( files );
-  }
-
-  function handleDragOver(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-  }
-
-  // Setup the dnd listeners.
-  var dropZone = document.getElementById('drop_zone');
-  dropZone.addEventListener('dragover', handleDragOver, false);
-  dropZone.addEventListener('drop', handleFileDrop, false);
+document.getElementById('inputFile').addEventListener('change', handleFileSelect, false);
 
 
 
+function handleFileDrop(evt) {
+  console.log( 'handleFileDrop evt: ', evt );
+  evt.stopPropagation();
+  evt.preventDefault();
 
-  function procesarFiles( files ) {
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-    console.log( f );
+  var files = evt.dataTransfer.files; // FileList object.
+  procesarFiles( files );
+}
 
-      // Only process image files.
-      if (f.type.match('image.*')) {
-        var reader = new FileReader();
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
 
-        // Closure to capture the file information.
-        reader.onload = (function cargado(theFile) {
-          return function procesando(e) {
-            console.log( e );
-            // Render thumbnail.
-            var span = document.createElement('div');
-            span.innerHTML = ['<strong>', escape(theFile.name), '</strong> (',
-              theFile.type || 'n/a', ') - ',
-              theFile.size, ' bytes, last modified: ',
-              theFile.lastModifiedDate ? theFile.lastModifiedDate.toLocaleDateString() : 'n/a',
-              '<br><img border="1" style="max-width:50px; max-height:50px;" src="', e.target.result,
-              '" title="', escape(theFile.name), '"/>'].join('');
-            document.getElementById('list').insertBefore(span, null);
-          };
-        })(f);
+// Setup the dnd listeners.
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileDrop, false);
 
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
-      }
-      else {
-        var span = document.createElement('div');
-        span.innerHTML = ['<strong>', escape(f.name), '</strong> (',
-          f.type || 'n/a', ') - ',
-          f.size, ' bytes, last modified: ',
-          f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a'].join('');
-        document.getElementById('list').insertBefore(span, null);
-      }
 
-    } // for
 
-  } // function procesarFiles
+
+function procesarFiles( files ) {
+  console.log( 'procesarFiles files: ', files );
+  // Loop through the FileList and render image files as thumbnails.
+  for (var i = 0, f; f = files[i]; i++) {
+  console.log( f );
+
+    // Only process image files.
+    if (f.type.match('image.*')) {
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function cargado(theFile) {
+        return function procesando(e) {
+          console.log( e );
+          // Render thumbnail.
+          var span = document.createElement('div');
+          span.innerHTML = ['<strong>', escape(theFile.name), '</strong> (',
+            theFile.type || 'n/a', ') - ',
+            theFile.size, ' bytes, last modified: ',
+            theFile.lastModifiedDate ? theFile.lastModifiedDate.toLocaleDateString() : 'n/a',
+            '<br><img border="1" style="max-width:50px; max-height:50px;" src="', e.target.result,
+            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+    else {
+      var span = document.createElement('div');
+      span.innerHTML = ['<strong>', escape(f.name), '</strong> (',
+        f.type || 'n/a', ') - ',
+        f.size, ' bytes, last modified: ',
+        f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a'].join('');
+      document.getElementById('list').insertBefore(span, null);
+    }
+  } // for
+} // function procesarFiles
 
 
 
@@ -151,7 +153,6 @@ function abortHandler(event) {
 
 </script>
 
-<script src="http://modernizr.com/downloads/modernizr-latest.js"></script>
 
 <?php //phpinfo(); ?>
 
