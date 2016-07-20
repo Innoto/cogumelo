31d57bf872f5fb8c6cbdb9d14afd2ec1ffca638b
Jose M. Porto <jmporto@innoto.es>
2016-07-20 15:17:51 +0200

    Limpiando código

--- coreModules/i18nGetLang/i18nGetLang.php
+++ coreModules/i18nGetLang/i18nGetLang.php
@@ -13,28 +13,29 @@ class i18nGetLang extends Module
 
     $i = 0;
     $langsConf = Cogumelo::getSetupValue( 'lang:available' );
-    if( $langsConf ) {
-      foreach( $langsConf as $l => $lang ) {
-        $lang_array[$i] = $l;
-        $i = $i +1;
+    $patron = is_array( $langsConf ) ? implode( '|', array_keys( $langsConf ) ) : cogumeloGetSetupValue( 'lang:default' );
+    /*
+      if( $langsConf ) {
+        foreach( $langsConf as $l => $lang ) {
+          $lang_array[$i] = $l;
+          $i = $i +1;
+        }
+        for( $j=0; $j<$i; $j++ ) {
+          if( $j===0 ) {
+            $patron = $lang_array[$j];
+          }
+          else if( $j===$i-1 ) {
+            $patron = $patron.'|'.$lang_array[$j];
+          }
+          else {
+            $patron = $patron .'|'.$lang_array[$j];
+          }
+        }
       }
-
-      for( $j=0; $j<$i; $j++ ) {
-        if( $j===0 ) {
-          $patron = $lang_array[$j];
-        }
-        else if( $j===$i-1 ) {
-          $patron = $patron.'|'.$lang_array[$j];
-        }
-        else {
-          $patron = $patron .'|'.$lang_array[$j];
-        }
+      else {
+        $patron = Cogumelo::getSetupValue( 'lang:default' );
       }
-    }
-    else {
-      $patron = Cogumelo::getSetupValue( 'lang:default' );
-    }
-    // error_log( 'i18nGetLang patron: '.$patron );
+    */
 
     $this->addUrlPatterns( '#^('.$patron.')\/(.*)$#', 'noendview:GetLangView::setlang' );
     $this->addUrlPatterns( '#^('.$patron.')\/?()$#', 'noendview:GetLangView::setlang' );
