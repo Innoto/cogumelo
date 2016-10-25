38023a20652d45fb384bfb2cebc417f44999a797
Jose M. Porto <jmporto@innoto.es>
2016-10-25 10:54:56 +0200

    Reemplazo la configuración de CKEditor

--- coreModules/form/classes/view/templates/js/ckeditor-config.js
+++ coreModules/form/classes/view/templates/js/ckeditor-config.js
@@ -7,19 +7,21 @@ CKEDITOR.editorConfig = function( config ) {
     { name: 'insert' },
     { name: 'forms' },
     { name: 'tools' },
-    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
     { name: 'others' },
     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
     { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
     { name: 'colors' },
+    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
   ];
 
   // Remove some buttons provided by the standard plugins, which are
   // not needed in the Standard(s) toolbar.
-  config.removeButtons = 'Underline,Subscript,Superscript,Maximize,Source,Scayt';
+  config.removeButtons = 'Subscript,Superscript,Maximize,Scayt';
+
+  // Underline, Source
 
   // Set the most common block elements.
-  config.format_tags = 'p;h1;h2;h3;pre';
+  config.format_tags = 'p;h1;h2;h3;h4;h5;hr;pre';
 
   // Simplify the dialog windows.
   //config.removeDialogTabs = 'image:advanced;link:advanced';
