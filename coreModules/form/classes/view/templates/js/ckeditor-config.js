f6f7cdfc4d39fab7807bc4625be7fd0f7b1cafd8
Jose M. Porto <jmporto@innoto.es>
2016-10-26 13:59:19 +0200

    Bug CKEDITOR.editorConfig

--- coreModules/form/classes/view/templates/js/ckeditor-config.js
+++ coreModules/form/classes/view/templates/js/ckeditor-config.js
@@ -18,10 +18,8 @@ CKEDITOR.editorConfig = function( config ) {
   // not needed in the Standard(s) toolbar.
   config.removeButtons = 'Subscript,Superscript,Maximize,Scayt';
 
-  // Underline, Source
-
   // Set the most common block elements.
-  config.format_tags = 'p;h1;h2;h3;h4;h5;hr;pre';
+  config.format_tags = 'p;h1;h2;h3;h4;h5;pre';
 
   // Simplify the dialog windows.
   //config.removeDialogTabs = 'image:advanced;link:advanced';
