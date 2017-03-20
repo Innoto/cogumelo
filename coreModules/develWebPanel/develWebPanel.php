d7fd9f8cd039eb9fe1484a45e707d5fd41d15753
pablinhob <pablinho.blanco@gmail.com>
2017-03-20 12:49:27 +0100

    comprobación de módulo devel cargado dende develWebPanel

--- coreModules/develWebPanel/develWebPanel.php
+++ coreModules/develWebPanel/develWebPanel.php
@@ -1,7 +1,5 @@
 <?php
 
-// Dependencias en classes/view/templates/js/devel.js
-
 Cogumelo::load("coreController/Module.php");
 
 class develWebPanel extends Module {
@@ -13,12 +11,16 @@ class develWebPanel extends Module {
   public function __construct() {
 
     global $COGUMELO_INSTANCED_MODULES;
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'$#', 'view:DevelView::main' );
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/read_logs$#', 'view:DevelView::read_logs' );
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_debugger#', 'view:DevelView::get_debugger' );
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_sql_tables$#', 'view:DevelView::get_sql_tables' );
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo$#', 'view:DevelView::develPhpInfo' );
-    $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo[/\#\?]+.*#', 'view:DevelView::develPhpInfo' );
+
+    if(  isset( $COGUMELO_INSTANCED_MODULES['devel'] )) {
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'$#', 'view:DevelView::main' );
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/read_logs$#', 'view:DevelView::read_logs' );
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_debugger#', 'view:DevelView::get_debugger' );
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_sql_tables$#', 'view:DevelView::get_sql_tables' );
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo$#', 'view:DevelView::develPhpInfo' );
+      $COGUMELO_INSTANCED_MODULES['devel']->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo[/\#\?]+.*#', 'view:DevelView::develPhpInfo' );
+
+    }
   }
 
 }
