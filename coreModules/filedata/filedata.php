2d11243afca0e5c1c4b1a3f64f3ff1614d9a7c5c
Jose M. Porto <jmporto@innoto.es>
2017-04-24 16:59:57 +0200

    aKey size 6

--- coreModules/filedata/filedata.php
+++ coreModules/filedata/filedata.php
@@ -14,9 +14,10 @@ class filedata extends Module {
 
 
   public function __construct() {
-    $this->addUrlPatterns( '#^cgmlImg/(?P<fileId>\d+?)(?P<aKey>-a[^\/]+?)?(?P<profile>/.+?)?(?P<fileName>/.*)?$#', 'view:FiledataImagesView::showImg' );
-    $this->addUrlPatterns( '#^cgmlformfilews/(?P<fileId>\d+)(?P<aKey>-a[^\/]+?)?(?P<fileName>/.*)?$#', 'view:FiledataWeb::webFormFileShow' );
-    $this->addUrlPatterns( '#^cgmlformfilewd/(?P<fileId>\d+)(?P<aKey>-a[^\/]+?)?(?P<fileName>/.*)?$#', 'view:FiledataWeb::webFormFileDownload' );
+    // Ej: cgmlImg/5-a2k3m4n/fastCut/algo.jpg
+    $this->addUrlPatterns( '#^cgmlImg/(?P<fileId>\d+?)(?P<aKey>-a[^\/]{6}?)(?P<profile>/.+?)?(?P<fileName>/.*)?$#', 'view:FiledataImagesView::showImg' );
+    $this->addUrlPatterns( '#^cgmlformfilews/(?P<fileId>\d+)(?P<aKey>-a[^\/]{6})?(?P<fileName>/.*)?$#', 'view:FiledataWeb::webFormFileShow' );
+    $this->addUrlPatterns( '#^cgmlformfilewd/(?P<fileId>\d+)(?P<aKey>-a[^\/]{6})?(?P<fileName>/.*)?$#', 'view:FiledataWeb::webFormFileDownload' );
     $this->addUrlPatterns( '#^cgmlformpublic/(.*)$#', 'view:FiledataWeb::webFormPublic' );
     // $this->addUrlPatterns( '#^cgmlfilews/(\d+).*$#', 'view:FiledataWeb::webShow' );
     // $this->addUrlPatterns( '#^cgmlfilewd/(\d+).*$#', 'view:FiledataWeb::webDownload' );
