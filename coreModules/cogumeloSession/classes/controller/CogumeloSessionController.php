33d2ed74840fdbf6556d8bae147f5b4edd956c4d
Jose M. Porto <jmporto@innoto.es>
2017-10-03 16:16:22 +0200

    No se ejecutan metodos de session en php modo cliente

--- coreModules/cogumeloSession/classes/controller/CogumeloSessionController.php
+++ coreModules/cogumeloSession/classes/controller/CogumeloSessionController.php
@@ -16,6 +16,12 @@ class CogumeloSessionController {
    * Constructor. Crea el TokenSessionID o lo carga del entorno y lo asigna a $C_SESSION_ID.
    */
   public function __construct() {
+
+    // $sessionSavePath = session_save_path();
+    // if( !is_dir ( $sessionSavePath ) ) {
+    //   mkdir( $sessionSavePath, 0700, true );
+    // }
+
     global $C_SESSION_ID;
     if( isset( $C_SESSION_ID ) ) {
       $this->tokenSessionID = $C_SESSION_ID;
@@ -34,73 +40,77 @@ class CogumeloSessionController {
     // error_log( '(Notice) prepareTokenSessionEnvironment INI' );
     // error_log( '$_COOKIE = '.json_encode($_COOKIE) );
 
-    session_name( $tkName );
+    // No se ejecuta en php modo cliente
+    if( php_sapi_name() !== 'cli' ) {
+
+      session_name( $tkName );
 
 
-    if( isset( $_COOKIE[ $tkName ] ) ) {
-      $tkSID = $_COOKIE[ $tkName ];
-    }
-    else {
-      if( isset( $_POST[ $tkName ] ) && trim( $_POST[ $tkName ] ) !== '' ) {
-        $tkSID = $_POST[ $tkName ];
-      }
-      elseif( isset( $_SERVER[ 'HTTP_X_'.$tkName ] ) && trim( $_SERVER[ 'HTTP_X_'.$tkName ] ) !== '' ) {
-        $tkSID = $_SERVER[ 'HTTP_X_'.$tkName ];
-      }
-    }
-
-
-    if( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
-      $remoteAddr = $_SERVER['HTTP_X_REAL_IP'];
-    }
-    elseif( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-      $remoteAddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else {
-      $remoteAddr = $_SERVER['REMOTE_ADDR'];
-    }
-
-
-    if( $tkSID ) {
-      session_id( $tkSID );
-      session_start();
-      if( isset( $_SESSION[ 'cogumeloSessionNew' ] ) ) {
-        $this->tokenSessionID = session_id();
-        $_SESSION[ 'cogumeloSessionNew' ] = false;
-        $_SESSION[ 'cogumeloSessionTimePrev' ] = ( $_SESSION[ 'cogumeloSessionTimeLast' ] ) ?
-          $_SESSION[ 'cogumeloSessionTimeLast' ] : $_SESSION[ 'cogumeloSessionTimeCreate' ];
-        $_SESSION[ 'cogumeloSessionTimeLast' ] = time();
-        if( $_SESSION[ 'cogumeloSessionRemoteAddr' ] !== $remoteAddr ) {
-          $_SESSION[ 'cogumeloSessionRemoteAddrPrev' ] = $_SESSION[ 'cogumeloSessionRemoteAddr' ];
-          $_SESSION[ 'cogumeloSessionRemoteAddrChange' ] = true;
-          $_SESSION[ 'cogumeloSessionRemoteAddr' ] = $remoteAddr;
-        }
+      if( isset( $_COOKIE[ $tkName ] ) ) {
+        $tkSID = $_COOKIE[ $tkName ];
       }
       else {
-        Cogumelo::debug( 'CogumeloSessionController: ## Invalid TokenSessionID' );
-        Cogumelo::debug( 'CogumeloSessionController: ## $_SESSION = '.json_encode($_SESSION) );
-        session_unset();
-        session_destroy();
-        $tkSID = false;
+        if( isset( $_POST[ $tkName ] ) && trim( $_POST[ $tkName ] ) !== '' ) {
+          $tkSID = $_POST[ $tkName ];
+        }
+        elseif( isset( $_SERVER[ 'HTTP_X_'.$tkName ] ) && trim( $_SERVER[ 'HTTP_X_'.$tkName ] ) !== '' ) {
+          $tkSID = $_SERVER[ 'HTTP_X_'.$tkName ];
+        }
       }
-    }
 
 
-    // $tkSID puede borrarse en el anterior if() invalidando el ID obtenido y dando lugar a uno nuevo
-    if( !$tkSID ) {
-      session_start();
-      session_regenerate_id(true);
-      // error_log( '(Notice) NEW TokenSessionID -> NEW session' );
-      $this->tokenSessionID = session_id();
-      $_SESSION[ 'cogumeloSessionId' ] = $this->tokenSessionID;
-      $_SESSION[ 'cogumeloSessionNew' ] = true;
-      $_SESSION[ 'cogumeloSessionTimeCreate' ] = time();
-      $_SESSION[ 'cogumeloSessionTimePrev' ] = false;
-      $_SESSION[ 'cogumeloSessionTimeLast' ] = false;
-      $_SESSION[ 'cogumeloSessionRemoteAddr' ] = $remoteAddr;
-      $_SESSION[ 'cogumeloSessionRemoteAddrPrev' ] = false;
-      $_SESSION[ 'cogumeloSessionRemoteAddrChange' ] = false;
-    }
+      if( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
+        $remoteAddr = $_SERVER['HTTP_X_REAL_IP'];
+      }
+      elseif( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+        $remoteAddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      }
+      else {
+        $remoteAddr = $_SERVER['REMOTE_ADDR'];
+      }
+
+
+      if( $tkSID ) {
+        session_id( $tkSID );
+        session_start();
+        if( isset( $_SESSION[ 'cogumeloSessionNew' ] ) ) {
+          $this->tokenSessionID = session_id();
+          $_SESSION[ 'cogumeloSessionNew' ] = false;
+          $_SESSION[ 'cogumeloSessionTimePrev' ] = ( $_SESSION[ 'cogumeloSessionTimeLast' ] ) ?
+            $_SESSION[ 'cogumeloSessionTimeLast' ] : $_SESSION[ 'cogumeloSessionTimeCreate' ];
+          $_SESSION[ 'cogumeloSessionTimeLast' ] = time();
+          if( $_SESSION[ 'cogumeloSessionRemoteAddr' ] !== $remoteAddr ) {
+            $_SESSION[ 'cogumeloSessionRemoteAddrPrev' ] = $_SESSION[ 'cogumeloSessionRemoteAddr' ];
+            $_SESSION[ 'cogumeloSessionRemoteAddrChange' ] = true;
+            $_SESSION[ 'cogumeloSessionRemoteAddr' ] = $remoteAddr;
+          }
+        }
+        else {
+          Cogumelo::debug( 'CogumeloSessionController: ## Invalid TokenSessionID' );
+          Cogumelo::debug( 'CogumeloSessionController: ## $_SESSION = '.json_encode($_SESSION) );
+          session_unset();
+          session_destroy();
+          $tkSID = false;
+        }
+      }
+
+
+      // $tkSID puede borrarse en el anterior if() invalidando el ID obtenido y dando lugar a uno nuevo
+      if( !$tkSID ) {
+        session_start();
+        session_regenerate_id(true);
+        // error_log( '(Notice) NEW TokenSessionID -> NEW session' );
+        $this->tokenSessionID = session_id();
+        $_SESSION[ 'cogumeloSessionId' ] = $this->tokenSessionID;
+        $_SESSION[ 'cogumeloSessionNew' ] = true;
+        $_SESSION[ 'cogumeloSessionTimeCreate' ] = time();
+        $_SESSION[ 'cogumeloSessionTimePrev' ] = false;
+        $_SESSION[ 'cogumeloSessionTimeLast' ] = false;
+        $_SESSION[ 'cogumeloSessionRemoteAddr' ] = $remoteAddr;
+        $_SESSION[ 'cogumeloSessionRemoteAddrPrev' ] = false;
+        $_SESSION[ 'cogumeloSessionRemoteAddrChange' ] = false;
+      }
+    } // if( !== 'cli' )
 
 
     global $C_SESSION_ID;
