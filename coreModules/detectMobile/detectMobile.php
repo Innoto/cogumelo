c2f7a739327a967b6ce398387292f02f665f6969
Jose M. Porto <jmporto@innoto.es>
2017-10-03 09:40:21 +0200

    Se publican en JS mod:detectMobile:isMobile, mod:detectMobile:isTablet

--- coreModules/detectMobile/detectMobile.php
+++ coreModules/detectMobile/detectMobile.php
@@ -32,6 +32,9 @@ class detectMobile extends Module {
       $detect = new Mobile_Detect;
       Cogumelo::setSetupValue( 'mod:detectMobile:isMobile', $detect->isMobile() );
       Cogumelo::setSetupValue( 'mod:detectMobile:isTablet', $detect->isTablet() );
+
+      Cogumelo::mergeSetupValue( 'mod:mediaserver:publicConf:javascript:setupFields',
+        [ 'mod:detectMobile:isMobile', 'mod:detectMobile:isTablet' ] );
     }
     else {
       Cogumelo::error('Mobile_detect.php dpendence not found. Execute ./cogumelo installDependences to install it.');
