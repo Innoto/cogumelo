9d597c857ce087c9891aed39c15a8b771dbb6163
Adrian <adrian.kewelinho@gmail.com>
2018-02-27 17:41:39 +0100

    .

--- coreModules/user/classes/view/UserView.php
+++ coreModules/user/classes/view/UserView.php
@@ -254,8 +254,11 @@ class UserView extends View {
     if(!isset($data) || $data == ''){
       $form->setValidationRule( 'password', 'required' );
       $form->setValidationRule( 'password2', 'required' );
+      if(empty(Cogumelo::getSetupValue('mod:user:password:notStrength'))){
+        $form->setValidationRule( 'password', 'passwordStrength', true);
+      }
       $form->setValidationRule( 'password', 'equalTo', '#password2' );
-      $form->setValidationRule( 'password', 'passwordStrength', true);
+
     }
     $form->setValidationRule( 'avatar', 'minfilesize', 1024 );
     $form->setValidationRule( 'avatar', 'accept', 'image/jpeg,image/png' );
@@ -315,8 +318,11 @@ class UserView extends View {
     }
     $form->setValidationRule( 'password', 'required' );
     $form->setValidationRule( 'password2', 'required' );
+    if(empty(Cogumelo::getSetupValue('mod:user:password:notStrength'))){
+      $form->setValidationRule( 'password', 'passwordStrength', true);
+    }
     $form->setValidationRule( 'password2', 'equalTo', '#password' );
-    $form->setValidationRule( 'password', 'passwordStrength', true);
+
 
     $form->setEnterSubmit( true );
 
