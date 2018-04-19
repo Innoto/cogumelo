60f588ca603a108950ddcf20ab6743351134dc8f
Jose M. Porto <jmporto@innoto.es>
2018-04-19 14:27:31 +0200

    .

--- cogumelo-server.php
+++ cogumelo-server.php
@@ -1,23 +1,7 @@
 <?php
-
-
 /*
-
 Este script se lanza desde un php en el servidor web que prepara el entorno con la infomacion necesaria
-
-La preparacion de la informacion puede ser algo como esto:
-
-// define( 'WEB_BASE_PATH', getcwd() ); // Apache DocumentRoot
-// define( 'PRJ_BASE_PATH', realpath( WEB_BASE_PATH.'/..' ) ); // Project Path (normalmente contiene app/ httpdocs/ formFiles/)
-// define( 'APP_BASE_PATH', PRJ_BASE_PATH.'/app' ); // App Path
-
-// set_include_path( '.:'.APP_BASE_PATH ); // Include cogumelo core Location
-
-// require_once( 'conf/setup.php' );
-
 */
-
-
 
 // We check that the conexion comes from localhost
 if( $_SERVER['REMOTE_ADDR'] !== 'local_shell' && isset( $_SERVER['REMOTE_ADDR'] ) &&  isPrivateIp( $_SERVER['REMOTE_ADDR'] ) ) {
@@ -28,32 +12,13 @@ if( $_SERVER['REMOTE_ADDR'] !== 'local_shell' && isset( $_SERVER['REMOTE_ADDR'] 
   require_once( COGUMELO_LOCATION.'/coreClasses/coreController/Cache.php' );
   require_once( APP_BASE_PATH.'/Cogumelo.php' );
 
-
-  $par = $_GET['q'];
-  switch( $par ) {
-    case 'rotate_logs':
-      $dir = Cogumelo::getSetupValue( 'logs:path' );
-      $handle = opendir( $dir );
-      while( $file = readdir( $handle ) ) {
-        $file = $dir.'/'.$file;
-        if( is_file( $file ) ) {
-          $pos = strpos( $file, 'gz' );
-          if( $pos === false ){
-            $gzfile = $file.'-'.date( 'Ymd-Hms' ).'.gz';
-            $fp = gzopen( $gzfile, 'w9' );
-            gzwrite ( $fp, file_get_contents( $file ) );
-            gzclose( $fp );
-          }
-        }
-      }
-      break;
+  switch( $_GET['q'] ) {
     case 'flush':
-
-      // // Def: templates_c
+      // // Def: app/tmp/templates_c
       // rmdirRec( Cogumelo::getSetupValue('smarty:compilePath'), false );
-      // // Def: cgmlImg
+      // // Def: httpdocs/cgmlImg
       // rmdirRec( Cogumelo::getSetupValue('mod:filedata:cachePath'), false );
-      // // Def: mediaCache
+      // // Def: httpdocs/mediaCache
       // rmdirRec( Cogumelo::getSetupValue('mod:mediaserver:tmpCachePath'), false );
       // echo ' - Cogumelo File cache flush'."\n";
 
@@ -65,14 +30,36 @@ if( $_SERVER['REMOTE_ADDR'] !== 'local_shell' && isset( $_SERVER['REMOTE_ADDR'] 
         $opcacheReset = opcache_reset(); // ( ($opcacheReset) ? 'OK' : 'FAIL'
         echo ' - Cogumelo PHP cache flush: Done'."\n";
       }
-
       break;
 
-    case 'client_caches':
-      // Cogumelo::load( 'coreController/ModuleController.php' );
-      // require_once( ModuleController::getRealFilePath( 'mediaserver.php', 'mediaserver' ) );
-      // mediaserver::autoIncludes();
-      // CacheUtilsController::generateAllCaches();
+    // case 'client_caches':
+    //   Cogumelo::load( 'coreController/ModuleController.php' );
+    //   require_once( ModuleController::getRealFilePath( 'mediaserver.php', 'mediaserver' ) );
+    //   mediaserver::autoIncludes();
+    //   CacheUtilsController::generateAllCaches();
+    //   break;
+
+    // case 'rotate_logs':
+    //   $dir = Cogumelo::getSetupValue( 'logs:path' );
+    //   $handle = opendir( $dir );
+    //   while( $file = readdir( $handle ) ) {
+    //     $file = $dir.'/'.$file;
+    //     if( is_file( $file ) ) {
+    //       $pos = strpos( $file, 'gz' );
+    //       if( $pos === false ){
+    //         $gzfile = $file.'-'.date( 'Ymd-Hms' ).'.gz';
+    //         $fp = gzopen( $gzfile, 'w9' );
+    //         gzwrite ( $fp, file_get_contents( $file ) );
+    //         gzclose( $fp );
+    //       }
+    //     }
+    //   }
+    //   break;
+
+    default:
+      header( 'HTTP/1.0 403 Forbidden' );
+      echo( "You are forbidden!\n\nUnusual access to cogumelo-server\n" );
+      error_log('ERROR: cogumelo-server.php - Access forbidden');
       break;
 
   } // switch
@@ -84,37 +71,37 @@ else {
 }
 
 
-function rmdirRec( $dir, $removeContainer = true ) {
-  // error_log( "rmdirRec( $dir )" );
+// function rmdirRec( $dir, $removeContainer = true ) {
+//   // error_log( "rmdirRec( $dir )" );
 
-  $dir = rtrim( $dir, '/' );
-  if( !empty( $dir ) && strpos( $dir, PRJ_BASE_PATH ) === 0 && is_dir( $dir ) ) {
-    $dirElements = scandir( $dir );
-    if( !empty( $dirElements ) ) {
-      foreach( $dirElements as $object ) {
-        if( $object !== '.' && $object !== '..' ) {
-          if( is_dir( $dir.'/'.$object ) ) {
+//   $dir = rtrim( $dir, '/' );
+//   if( !empty( $dir ) && strpos( $dir, PRJ_BASE_PATH ) === 0 && is_dir( $dir ) ) {
+//     $dirElements = scandir( $dir );
+//     if( !empty( $dirElements ) ) {
+//       foreach( $dirElements as $object ) {
+//         if( $object !== '.' && $object !== '..' ) {
+//           if( is_dir( $dir.'/'.$object ) ) {
 
-            rmdirRec( $dir.'/'.$object );
-          }
-          else {
+//             rmdirRec( $dir.'/'.$object );
+//           }
+//           else {
 
-            unlink( $dir.'/'.$object );
-          }
-        }
-      }
-    }
-    reset( $dirElements );
-    if( $removeContainer ) {
-      if( !is_link( $dir ) ) {
-        rmdir( $dir );
-      }
-      else {
-        unlink( $dir );
-      }
-    }
-  }
-}
+//             unlink( $dir.'/'.$object );
+//           }
+//         }
+//       }
+//     }
+//     reset( $dirElements );
+//     if( $removeContainer ) {
+//       if( !is_link( $dir ) ) {
+//         rmdir( $dir );
+//       }
+//       else {
+//         unlink( $dir );
+//       }
+//     }
+//   }
+// }
 
 
 function isPrivateIp( $ip ) {
