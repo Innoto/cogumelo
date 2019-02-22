d8d5f63fa8ff27837a17fd2d9818488711aa8b87
Carlos Zas <czas@innoto.es>
2019-02-22 09:52:07 +0100

    Add to CKeditor extraPlugins (config): enable justify/showblocks

--- coreModules/form/classes/view/templates/js/ckeditor-config.js
+++ coreModules/form/classes/view/templates/js/ckeditor-config.js
@@ -26,6 +26,9 @@ CKEDITOR.editorConfig = function( config ) {
   // Set the most common block elements.
   config.format_tags = 'p;h1;h2;h3;h4;h5;pre';
 
+  // Se debe añadir extraPlugins (You need the Justify plugin since CKEditor 4)
+  config.extraPlugins = 'justify,showblocks';
+
   config.height = '150';
 };
 
